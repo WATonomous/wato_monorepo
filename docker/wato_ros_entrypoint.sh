8c3a22bc169b5377de4b17a7247f9d2ee953110b
Cheeran Mahalingam <cmahalingam@watonomous.ca>
2022-11-06 09:11:14 +0000

    Dockerfiles for sample nodes

--- docker/wato_ros_entrypoint.sh
+++ docker/wato_ros_entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# setup ros2 environment
-# echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> "/home/docker/.bashrc"
-# echo "source /home/docker/ament_ws/install/setup.bash" >> "/home/docker/.bashrc"
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/home/docker/ament_ws/install/setup.bash"
+# setup ROS environment
+source /home/docker/ament_ws/install/setup.bash
+echo "source /home/docker/ament_ws/install/setup.bash" >> ~/.bashrc
 
 exec "/usr/local/bin/fixuid" "-q" "$@"
