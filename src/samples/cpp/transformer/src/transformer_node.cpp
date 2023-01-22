66e863b89cf35f284b4e4e5d2c5db9eb49b4870f
Cheran Mahalingam <cmahalingam@watonomous.ca>
2023-01-22 05:35:40 +0000

    Add transformer log message

--- src/samples/cpp/transformer/src/transformer_node.cpp
+++ src/samples/cpp/transformer/src/transformer_node.cpp
@@ -35,6 +35,7 @@ void TransformerNode::unfiltered_callback(const sample_msgs::msg::Unfiltered::Sh
     this->get_parameter("compression_method").as_int();
 
   if (transformer_.enqueue_message(filtered)) {
+    RCLCPP_INFO(this->get_logger(), "Buffer Capacity Reached. PUBLISHING...\n");
     // Publish processed data when the buffer reaches its capacity
     sample_msgs::msg::FilteredArray filtered_msgs;
     auto buffer = transformer_.buffer_messages();
