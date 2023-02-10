3203873d8873b5d5b080828ca82b63ce33725f31
Rijin <r9murali@watonomous.ca>
2023-02-10 03:08:31 +0000

    modified ros launch file and updated code structure

--- src/sensor_interfacing/ARSPointCloudFilter/src/ars_pointcloud_filter.cpp
+++ src/sensor_interfacing/ARSPointCloudFilter/src/ars_pointcloud_filter.cpp
@@ -3,16 +3,15 @@
 #include "ars_pointcloud_filter.hpp"
 
 
-// unfiltered left and right ars (how is this combined)?
 radar_msgs::msg::RadarPacket ARSPointCloudFilter::snr_filter(const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars,
                                        double snr_threshold)
-{   
+{
     radar_msgs::msg::RadarPacket filtered_ars;
-    for (auto detection:unfiltered_ars->detections)
+    for (auto detection : unfiltered_ars->detections)
     {
         if(detection.snr < snr_threshold)
         {
-            //an array of filtered objects
+            // an array of filtered objects
             filtered_ars.detections.push_back(detection);
         }
     }
@@ -20,30 +19,29 @@ radar_msgs::msg::RadarPacket ARSPointCloudFilter::snr_filter(const radar_msgs::m
 }
 
 radar_msgs::msg::RadarPacket ARSPointCloudFilter::azimuth_angle_filter(const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars,
-                                                double AzAng0_threshold, double AzAng1_threshold)           
+                                                double AzAng0_threshold, double AzAng1_threshold)       
 {
     radar_msgs::msg::RadarPacket filtered_ars;
-    for (auto detection:unfiltered_ars->detections)
+    for (auto detection : unfiltered_ars->detections)
     {
         if(abs(detection.az_ang0) < abs(AzAng0_threshold) && detection.az_ang1 < abs(AzAng1_threshold))
         {
-            //point angles are within the defined constraints
+            // point angles are within the defined constraints
             filtered_ars.detections.push_back(detection);
         }
     }
     return filtered_ars;
-
 }
 
 radar_msgs::msg::RadarPacket ARSPointCloudFilter::range_filter(const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars,
                                        double range_threshold)
 {
     radar_msgs::msg::RadarPacket filtered_ars;
-    for (auto detection:unfiltered_ars->detections)
+    for (auto detection : unfiltered_ars->detections)
     {
         if(detection.range < range_threshold)
         {
-            //an array of filtered objects
+            // an array of filtered objects
             filtered_ars.detections.push_back(detection);
         }
     }
