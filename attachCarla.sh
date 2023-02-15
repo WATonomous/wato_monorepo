351cd3bc29b941ae48015187b7ad30db65ffa631
Shahan Neda <shahan.neda@gmail.com>
2023-02-15 04:44:09 +0000

    Fixed VNC with simulation repos

---
+++ attachCarla.sh
@@ -0,0 +1,1 @@
+docker exec -i -t --user root watod_snedadah_carla bash 
---
+++ docker/wato_ros_entrypoint_foxy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# setup ROS2 environment
+source /home/docker/colcon_ws/install/setup.bash
+
+exec "$@"
