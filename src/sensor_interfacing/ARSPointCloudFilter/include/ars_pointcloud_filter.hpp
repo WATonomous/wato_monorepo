9f63e8020a207c479485b63255acb9acf588f7c8
Rijin <r9murali@watonomous.ca>
2023-02-22 02:50:47 +0000

    created near scan and far scan filters

--- src/sensor_interfacing/ARSPointCloudFilter/include/ars_pointcloud_filter.hpp
+++ src/sensor_interfacing/ARSPointCloudFilter/include/ars_pointcloud_filter.hpp
@@ -29,34 +29,14 @@ public:
   
   void near_scan_filter(const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars,
                         radar_msgs::msg::RadarPacket &buffer_packet, double vrel_rad_param, double el_ang_param,
-                        double rcs0_param, double snr_param, double range_param, double az_ang0_param);
+                        double rcs0_param, double snr_param, double range_param, double az_ang0_param, std::pair<bool, radar_msgs::msg::RadarPacket> &publish_packet);
 
-  // void far_scan_filter(const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars,
-  //                     radar_msgs::msg::RadarPacket &buffer_packet, struct ARSPointCloudFilterNode::filter_parameters parameters);
+  void far_scan_filter(const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars,
+                        radar_msgs::msg::RadarPacket &buffer_packet, double vrel_rad_param, double el_ang_param,
+                        double rcs0_param, double snr_param, double range_param, double az_ang0_param, std::pair<bool, radar_msgs::msg::RadarPacket> &publish_packet);
 
-
-
-
-
-
-
-  // radar_msgs::msg::RadarPacket snr_filter(
-  //   const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars, double snr_threshold);
-
-  // radar_msgs::msg::RadarPacket azimuth_angle_filter(
-  //   const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars, double AzAng0_threshold);
-
-  // radar_msgs::msg::RadarPacket range_filter(
-  //   const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars, double range_threshold);
-
-  // radar_msgs::msg::RadarPacket vrel_rad_filter(
-  //   const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars, double vrel_rad_threshold);
-
-  // radar_msgs::msg::RadarPacket el_ang_filter(
-  //   const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars, double el_ang_threshold);
-
-  // radar_msgs::msg::RadarPacket rcs_filter(
-  //   const radar_msgs::msg::RadarPacket::SharedPtr unfiltered_ars, double rcs_threshold);
 };
+
 }
+
 #endif  // ARS_POINTCLOUD_FILTER_HPP_
