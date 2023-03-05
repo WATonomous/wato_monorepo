3d3573f7b2b0e3c54e5f1c3d3c4e00c6d75e5a15
Justin Leung <j89leung@watonomous.ca>
2023-03-05 23:36:37 +0000

    Update .gitignore

--- build/sample_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leungjch/Documents/repos/wato_monorepo_v2/src/wato_msgs/sample_msgs
-# Build directory: /home/leungjch/Documents/repos/wato_monorepo_v2/build/sample_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
