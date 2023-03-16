bddb4b14d3f34b6e5d3f79169ad49d3fabb7fc86
Steven Gong <s36gong@uwaterloo.ca>
2023-03-16 00:56:01 +0000

    add launch and config files for camera_detection

---
+++ src/camera_detection/launch/sim_launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    config = os.path.join(
+        get_package_share_directory('camera_detection'),
+        'config',
+        'sim_config.yaml'
+    )
+
+    # nodes
+    camera_detection_node = Node(
+        package='camera_detection',
+        executable='camera_detection_node',
+        name='camera_detection_node',
+        parameters=[config]
+    )
+
+    # finalize
+    ld.add_action(camera_detection_node)
+
+    return ld
