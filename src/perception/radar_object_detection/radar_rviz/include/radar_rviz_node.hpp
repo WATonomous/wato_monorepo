d4794f85b53c0788f1315c827ddedc6dc24fdc69
Rijin <r9murali@watonomous.ca>
2023-03-31 22:49:46 +0000

    added sensor_msgs and pseudocode for point cloud data conversion

--- src/perception/radar_object_detection/radar_rviz/include/radar_rviz_node.hpp
+++ src/perception/radar_object_detection/radar_rviz/include/radar_rviz_node.hpp
@@ -2,6 +2,8 @@
 #define RADAR_RVIZ_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/point_field.hpp"
 
 #include "radar_msgs/msg/radar_packet.hpp"
 #include "radar_msgs/msg/radar_detection.hpp"
@@ -28,6 +30,9 @@ private:
 
   // ROS2 Subscriber listening to "processed" topic.
   rclcpp::Subscription<radar_msgs::msg::RadarPacket>::SharedPtr raw_sub_;
+
+  // ROS2 Publisher that sends point cloud data to "visualization" topic for Rviz
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr raw_pub_;
 
   // An object containing methods for converting radar packets into point clouds.
   processing::RadarRvizProcessor packet_to_rviz_processor_;
