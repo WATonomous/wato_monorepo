41661d61b949747c4c586e57cb8dc54e560994d9
Dwbcan <dew.bhaumik8@gmail.com>
2023-04-11 09:04:32 +0000

    Created env_model branch and env_model node to create lanelet map and get ego lanelets

--- src/world_modeling/hd_map/sample/src/sample_node.cpp
+++ src/world_modeling/hd_map/sample/src/sample_node.cpp
@@ -1,40 +1,78 @@
+#include <chrono>
 #include <memory>
+#include <string>
+#include <vector>
 
-#include "sample_node.hpp"
+#include "lanelet2_core/LaneletMap.h"
+#include "lanelet2_core/io/XmlIO.h"
+#include "lanelet2_routing/RoutingGraph.h"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_listener.h"
 
-SampleNode::SampleNode()
-: Node("sample"), sample_(world_modeling::hd_map::Sample())
-{
-  sample_sub_ = this->create_subscription<sample_msgs::msg::Unfiltered>(
-    "unfiltered", ADVERTISING_FREQ,
-    std::bind(
-      &SampleNode::sample_sub_callback, this,
-      std::placeholders::_1));
+using DistLanelet = std::pair<double, lanelet::ConstLanelet>;
+using DistLaneletList = std::vector<DistLanelet>;
 
-  sample_pub_ =
-    this->create_publisher<sample_msgs::msg::Unfiltered>("hd_map_sample_topic", ADVERTISING_FREQ);
+class EnvModel : public rclcpp::Node {
+public:
+  EnvModel() : Node("env_model") {
+    // Load lanelet map
+    lanelet::io_handlers::XmlReader reader;
+    lanelet::ErrorMessages errors;
+    lanelet::LaneletMapPtr map = reader("lanelet_map.osm", errors);
+    if (!errors.empty()) {
+      throw std::runtime_error("Failed to load map");
+    }
 
-  // Define the default values for parameters if not defined in params.yaml
-  this->declare_parameter("version", rclcpp::ParameterValue(0));
-  this->declare_parameter("compression_method", rclcpp::ParameterValue(0));
-}
+    lanelet_map_ = map;
+    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-void SampleNode::sample_sub_callback(const sample_msgs::msg::Unfiltered::SharedPtr msg)
-{
-  this->sample_publish(msg);
-}
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(1000),
+                                     std::bind(&EnvModel::getEgoLanelets, this));
+  }
 
-void SampleNode::sample_publish(const sample_msgs::msg::Unfiltered::SharedPtr msg)
-{
-  auto pub_msg = sample_msgs::msg::Unfiltered(); 
-  RCLCPP_INFO(this->get_logger(), "Publishing Sample Message from HD Map...\n");
-  sample_pub_->publish(pub_msg);
-}
+  DistLaneletList getEgoLanelets() {
+    geometry_msgs::msg::TransformStamped tf;
+    try {
+      tf = tf_buffer_->lookupTransform(kMapFrameName, kVehicleFrameName, tf2::TimePoint(), std::chrono::duration<double>(5.0));
+    } catch (tf2::TransformException &ex) {
+      return {};
+    }
+  
+    auto ego_lanelets = lanelet::geometry::findWithin2d(
+        lanelet_map_->laneletLayer,
+        lanelet::Point2d{lanelet::InvalId, tf.transform.translation.x,
+                         tf.transform.translation.y});
 
-int main(int argc, char ** argv)
-{
+    DistLaneletList dist_lanelets;
+    for (const auto &lanelet : ego_lanelets) {
+      double distance = lanelet::geometry::distance(lanelet, tf.transform.translation.x,
+                                                     tf.transform.translation.y);
+      dist_lanelets.emplace_back(distance, lanelet);
+    }
+
+    // sort by distance
+    std::sort(dist_lanelets.begin(), dist_lanelets.end(),
+              [](const DistLanelet &lhs, const DistLanelet &rhs) {
+                return lhs.first < rhs.first;
+              });
+
+    return dist_lanelets;
+  }
+
+private:
+  lanelet::LaneletMapPtr lanelet_map_;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  const std::string kMapFrameName = "map";
+  const std::string kVehicleFrameName = "vehicle";
+};
+
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<SampleNode>());
+  rclcpp::spin(std::make_shared<EnvModel>());
   rclcpp::shutdown();
   return 0;
 }
