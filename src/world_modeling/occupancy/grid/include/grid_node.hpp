65b37a87b3e2631598f7d6ec156184328fab5fe2
jmogen <jmogen@uwaterloo.ca>
2023-04-12 00:14:01 +0000

    message type updates, forgot to push this last week

--- src/world_modeling/occupancy/grid/include/grid_node.hpp
+++ src/world_modeling/occupancy/grid/include/grid_node.hpp
@@ -2,18 +2,19 @@
 #define WORLD_MODELING_OCCUPANCY_GRID_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
-#include "sample_msgs/msg/filtered.hpp"
-#include "sample_msgs/msg/filtered_array.hpp"
-#include "sample_msgs/msg/unfiltered.hpp"
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+// #include <ros/ros.h>
+// #include "sample_msgs/msg/pointcloud2.hpp"
+// #include "sample_msgs/msg/occupancygrid.hpp"
 
 #include "grid.hpp"
 
 /**
- * Implementation of a ROS2 node that converts unfiltered messages to filtered_array
+ * Implementation of a ROS2 node that converts lidar messages to occupacygrid
  * messages.
  *
- * Listens to the "unfiltered" topic and filters out data with invalid fields
+ * Listens to the "LIDAR_TOP" topic and filters out data with invalid fields
  * and odd timestamps. Once the node collects BUFFER_CAPACITY messages it packs
  * the processed messages into an array and publishes it to the "filtered" topic.
  */
@@ -34,19 +35,19 @@ private:
    * A ROS2 subscription node callback used to process raw data from the
    * "unfiltered" topic and publish to the "filtered" topic.
    *
-   * @param msg a raw message from the "unfiltered" topic
+   * @param msg a raw message from the "LIDAR_TOP" topic
    */
   void grid_sub_callback(
-    const sample_msgs::msg::Unfiltered::SharedPtr msg);
+    const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
   void grid_publish(
-    const sample_msgs::msg::Unfiltered::SharedPtr msg);
+    const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
-  // ROS2 subscriber listening to the unfiltered topic.
-  rclcpp::Subscription<sample_msgs::msg::Unfiltered>::SharedPtr grid_sub_;
+  // ROS2 subscriber listening to the LIDAR_TOP topic.
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr grid_sub_;
 
-  // ROS2 publisher sending processed messages to the filtered topic.
-  rclcpp::Publisher<sample_msgs::msg::Unfiltered>::SharedPtr grid_pub_;
+  // ROS2 publisher sending processed messages to the occupancygrid topic.
+  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr grid_pub_;
 
   // Object that handles data processing and validation.
   world_modeling::occupancy::Grid grid_;
--- src/world_modeling/occupancy/grid/src/grid.cpp
+++ src/world_modeling/occupancy/grid/src/grid.cpp
@@ -10,11 +10,11 @@ namespace world_modeling::occupancy
 
     bool Grid::arbitrary_occupancy(
         // the message needs to be replaced with the lidar message which does not exist yet from what I can see
-        const sample_msgs::msg::THIS NEEDS TO BE CHANGED::SharedPtr msg,
-        sample_msgs::msg::OccupancyGrid & occupancy_grid)
+        const sensor_msgs::msg::PointCloud2::SharedPtr msg,
+        nav_msgs::msg::OccupancyGrid & occupancy_grid)
     {
         // Some arbitrary transformation of lidar data to occupancy grid
-
+        return true;
     }
 
 }  // namespace world_modeling::hd_map
