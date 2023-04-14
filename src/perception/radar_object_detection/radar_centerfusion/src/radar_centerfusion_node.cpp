73d5a4ca25fa2a519679ecf82cc738a0f60519ee
Rijin <r9murali@watonomous.ca>
2023-04-14 02:45:39 +0000

    added base node files for centerfusion inference

---
+++ src/perception/radar_object_detection/radar_centerfusion/src/radar_centerfusion_node.cpp
@@ -0,0 +1,31 @@
+
+#include <chrono>
+#include <memory>
+
+#include "radar_centerfusion_node.hpp"
+#include "radar_centerfusion.hpp"
+
+RadarCenterFusionNode::RadarCenterFusionNode()
+: Node("radar_centerfusion")
+{
+  raw_sub_ = this->create_subscription<radar_msgs::msg::RadarPacket>(
+    "radar_data",
+    1, std::bind(
+      &RadarCenterFusionNode::radar_data_callback,
+      this, std::placeholders::_1));
+  raw_pub_ = this->create_publisher<radar_msgs::msg::RadarPacket>("centerfusion_inference", 20);
+}
+
+void RadarRvizProcessorNode::radar_data_callback(
+  const radar_msgs::msg::RadarPacket::SharedPtr msg)
+{
+
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<RadarCenterFusionNode>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/perception/radar_object_detection/radar_centerfusion/test/radar_rviz_test.cpp
@@ -0,0 +1,15 @@
+#include <memory>
+#include <string>
+#include <tuple>
+
+#include "gtest/gtest.h"
+#include "rclcpp/rclcpp.hpp"
+
+#include "radar_centerfusion.hpp"
+#include "radar_centerfusion_node.hpp"
+
+class RadarCenterFusionNodeFixtureTest : public ::testing::Test
+{
+public:
+  void SetUp() {}
+};
