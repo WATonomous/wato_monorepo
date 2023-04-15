7a43cc8e2e4a1fa576fce2c37124307dffeadf3d
Dwbcan <dew.bhaumik8@gmail.com>
2023-04-15 08:16:38 +0000

    Merged changes from world-modeling branch

---
+++ src/world_modeling/hd_map/src/env_model.cpp
@@ -0,0 +1,92 @@
+#include <chrono>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "lanelet2_core/LaneletMap.h"
+#include "lanelet2_core/io/XmlIO.h"
+#include "lanelet2_routing/RoutingGraph.h"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_listener.h"
+
+using DistLanelet = std::pair<double, lanelet::ConstLanelet>;
+using DistLaneletList = std::vector<DistLanelet>;
+
+class EnvModel : public rclcpp::Node {
+public:
+  EnvModel() : Node("env_model") {
+   // create a Lanelet2 map
+    lanelet::LaneletMapPtr map = std::make_shared<lanelet::LaneletMap>();
+
+    // create 5 random lanelets
+    for (int i = 0; i < 5; ++i) {
+        // create some random points
+        lanelet::Point3d point1(i*20, 0, 0);
+        lanelet::Point3d point2(i*20 + 10, 0, 0);
+        lanelet::Point3d point3(i*20 + 10, 10, 0);
+        lanelet::Point3d point4(i*20, 10, 0);
+
+        // create a polygon from the points
+        lanelet::Polygon3d polygon({point1, point2, point3, point4});
+
+        // create a lanelet from the polygon
+        lanelet::LineString3d centerline({point1, point2, point3, point4});
+        lanelet::Lanelet lanelet(lanelet::utils::getId(), centerline, {polygon});
+
+        // add the lanelet to the map
+        map->add(lanelet);
+    }
+
+    lanelet_map_ = map;
+    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(1000),
+                                     std::bind(&EnvModel::getEgoLanelets, this));
+  }
+
+  DistLaneletList getEgoLanelets() {
+    geometry_msgs::msg::TransformStamped tf;
+    try {
+      tf = tf_buffer_->lookupTransform(kMapFrameName, kVehicleFrameName, tf2::TimePoint(), std::chrono::duration<double>(5.0));
+    } catch (tf2::TransformException &ex) {
+      return {};
+    }
+  
+    auto ego_lanelets = lanelet::geometry::findWithin2d(
+        lanelet_map_->laneletLayer,
+        lanelet::Point2d{lanelet::InvalId, tf.transform.translation.x,
+                         tf.transform.translation.y});
+
+    DistLaneletList dist_lanelets;
+    for (const auto &lanelet : ego_lanelets) {
+      double distance = lanelet::geometry::distance(lanelet, tf.transform.translation.x,
+                                                     tf.transform.translation.y);
+      dist_lanelets.emplace_back(distance, lanelet);
+    }
+
+    // sort by distance
+    std::sort(dist_lanelets.begin(), dist_lanelets.end(),
+              [](const DistLanelet &lhs, const DistLanelet &rhs) {
+                return lhs.first < rhs.first;
+              });
+
+    return dist_lanelets;
+  }
+
+private:
+  lanelet::LaneletMapPtr lanelet_map_;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  const std::string kMapFrameName = "map";
+  const std::string kVehicleFrameName = "vehicle";
+};
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<EnvModel>());
+  rclcpp::shutdown();
+  return 0;
+}
