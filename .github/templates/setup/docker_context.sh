5c3599b1edc475ae3f15f10c671634929b2f66a5
Cheran Mahalingam <cheranm@outlook.com>
2023-05-01 03:05:37 +0000

    github actions ci

---
+++ .github/templates/setup/docker_context.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+bash dev_config.sh
+
+profiles=$(find profiles -name "docker-compose*")
+dockerfile_paths=()
+services=()
+while read -r profile; do
+  profile_services=$(docker-compose -f "$profile" config --services)
+
+  while read -r service; do
+    # Parse yaml for docker-compose service and retrieve dockerfile path
+    service_config=$(docker-compose -f "$profile" config | yq ".services.$service")
+    dockerfile_path=$(echo "$service_config" | yq '.build.dockerfile')
+
+    dockerfile_paths+=("$dockerfile_path")
+    services+=("$service")
+  done <<< "$profile_services"
+done <<< "$profiles"
+
+# Combine dockerfile paths into array of json dicts
+# e.g. [{path: ...}, {path: ...}, ...]
+json_paths=$(jq -n '$ARGS.positional' --args -- "${dockerfile_paths[@]}" | \
+  jq -c 'map({path: .})')
+# Combine docker-compose services into array of json dicts
+# e.g. [{service: samples}, {service: vis_tools}, ...]
+json_services=$(jq -n '$ARGS.positional' --args -- "${services[@]}" | \
+  jq -c 'map({service: .})')
+# Zip previous arrays of json dicts for dockerfile path and services into one list
+# e.g. [{path: ..., service: samples}, {path: ..., service: vis_tools}, ...]
+json_context=$(jq -nc \
+  --argjson paths "$json_paths" \
+  --argjson services "$json_services" \
+  '[$paths, $services] | transpose | map(add)')
+echo "context=$json_context" >> "$GITHUB_OUTPUT"
+
+# Retrieve path of all docker-compose files
+# Remove profiles/docker-compose. and .yaml to get string of profiles
+# e.g. "samples vis_tools data_stream"
+profiles_string=$(find profiles -name "docker-compose*" | \
+  sed -e "s/^profiles\/docker-compose\.//" -e "s/\.yaml$//" | \
+  paste -sd " ")
+echo "profiles=$profiles_string" >> "$GITHUB_OUTPUT"
+
