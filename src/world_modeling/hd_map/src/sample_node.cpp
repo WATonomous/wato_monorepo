e48fa35360b310334ca427acaed3b7aecbb8b29a
Owen Leather <owenleather03@gmail.com>
2023-05-20 03:31:04 +0000

    lanelet to ros marker visualization tools

--- src/world_modeling/hd_map/src/sample_node.cpp
+++ src/world_modeling/hd_map/src/sample_node.cpp
@@ -6,10 +6,10 @@ SampleNode::SampleNode()
 : Node("sample"), sample_(world_modeling::hd_map::Sample()), routing_(world_modeling::hd_map::HDMapRouting())
 {
   sample_pub_ =
-    this->create_publisher<visualization_msgs::msg::Marker>("hd_map", ADVERTISING_FREQ);
+    this->create_publisher<visualization_msgs::msg::MarkerArray>("hd_map", ADVERTISING_FREQ);
 
   timer_ = this->create_wall_timer(
-      std::chrono::milliseconds(1009),
+      std::chrono::milliseconds(10000),
       std::bind(&SampleNode::sample_publish, this));
 
   // Define the default values for parameters if not defined in params.yaml
@@ -19,35 +19,12 @@ SampleNode::SampleNode()
 
 void SampleNode::sample_publish()
 {
-  lanelet::PointLayer& points = this->routing_.map_->pointLayer;
-
-  lanelet::Point3d aPoint = *points.begin();
-
-  std_msgs::msg::Header header; // empty header
-  header.stamp = rclcpp::Clock().now(); // time
+  lanelet::LineStringLayer& linestrings = this->routing_.map_->lineStringLayer;
   
-  auto marker = visualization_msgs::msg::Marker(); 
-  marker.header = header;
-  marker.id = 8;
-  
-  marker.color.a = 1.0; 
-  marker.color.r = 0.0;
-  marker.color.g = 1.0;
-  marker.color.b = 0.0;
-
-  std::vector<geometry_msgs::msg::Point> rosPoints;
-
-  auto pt = geometry_msgs::msg::Point();
-  pt.x = aPoint.x();
-  pt.y = aPoint.y();
-  pt.z = aPoint.z();
-
-  rosPoints.push_back(pt);
-
-  marker.points = rosPoints;
+  auto markerArray = world_modeling::hd_map::lineStringsAsMarkerArray(linestrings);  
 
   RCLCPP_INFO(this->get_logger(), "Publishing Lanelet Message from HD Map...\n");
-  sample_pub_->publish(marker);
+  sample_pub_->publish(markerArray);
 }
 
 int main(int argc, char ** argv)
