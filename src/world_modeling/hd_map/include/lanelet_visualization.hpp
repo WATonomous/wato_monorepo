b07389e1271315b29b9ec0fe7a6cd615fa61f9e3
Owen Leather <owenleather03@gmail.com>
2023-05-20 03:31:04 +0000

    lanelet to ros marker visualization tools

---
+++ src/world_modeling/hd_map/include/lanelet_visualization.hpp
@@ -0,0 +1,23 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
+#include "std_msgs/msg/header.hpp"
+#include "std_msgs/msg/color_rgba.hpp"
+#include "geometry_msgs/msg/point.hpp"
+
+#include <lanelet2_core/primitives/Lanelet.h>
+#include <lanelet2_io/Io.h>
+#include <lanelet2_projection/UTM.h>
+#include <lanelet2_routing/Route.h>
+#include <lanelet2_routing/RoutingCost.h>
+#include <lanelet2_routing/RoutingGraph.h>
+#include <lanelet2_routing/RoutingGraphContainer.h>
+#include <lanelet2_traffic_rules/TrafficRulesFactory.h>
+
+namespace world_modeling::hd_map{
+    visualization_msgs::msg::MarkerArray laneletMapAsMarkerArray(lanelet::LaneletMapPtr map);
+    visualization_msgs::msg::MarkerArray laneletAsMarkerArray(lanelet::Lanelet lanelet, int *id);
+    visualization_msgs::msg::MarkerArray lineStringsAsMarkerArray(lanelet::LineStringLayer& lineStrings);
+    visualization_msgs::msg::Marker lineStringAsMarker(lanelet::ConstLineString3d lineString, int *id, float thickness, int type, std_msgs::msg::ColorRGBA color);
+}
