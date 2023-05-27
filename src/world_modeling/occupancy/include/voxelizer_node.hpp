e251ba63f3c49c1fc2d89b54f487094baab3785b
jmogen <jmogen@uwaterloo.ca>
2023-05-27 00:01:08 +0000

    dependencies broken, too hungry to fix rn

--- src/world_modeling/occupancy/include/voxelizer_node.hpp
+++ src/world_modeling/occupancy/include/voxelizer_node.hpp
@@ -1,9 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
 
-#include "world_modeling_msgs/msg/voxel_grid.hpp" 
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include "world_modeling_msgs/msg/voxel_grid.hpp"
+
 
 class VoxelizerNode : public rclcpp::Node
 {
   public:
     VoxelizerNode();
+
+  private:
+  void VoxelizerNode::voxel_sub_callback(
+    const pcl::PCLPointCloud2::SharedPtr msg)
+
+  void VoxelizerNode::voxel_publish(
+    const pcl::VoxelGrid::SharedPtr msg)
+
+  rclcpp::Subscription<pcl::PCLPointCloud2>::SharedPtr pointcloud_sub_;
+
+  rclcpp::Publisher<pcl::VoxelGrid>::SharedPtr voxel_pub_;
 };
