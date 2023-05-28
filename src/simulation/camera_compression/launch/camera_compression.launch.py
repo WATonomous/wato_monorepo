9c1bceb8012edd41c7162e243a639da9d36e012b
VishGit1234 <vishalschool1234@gmail.com>
2023-05-28 21:46:53 +0000

    foxglove working temporary fix

--- src/simulation/camera_compression/launch/camera_compression.launch.py
+++ src/simulation/camera_compression/launch/camera_compression.launch.py
@@ -6,6 +6,7 @@ def generate_launch_description():
     camera_compression_node = Node(
         package='camera_compression',
         executable='camera_compression_node',
+        respawn=True
     )
 
     return LaunchDescription([
