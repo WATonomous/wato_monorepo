83d72b08210a3e1e31234bfd85011d40a839070d
Justin Leung <j89leung@watonomous.ca>
2023-05-30 03:29:05 +0000

    Add pointpillars repo

--- src/lidar_object_detection/src/lidar_detection_node.cpp
+++ src/lidar_object_detection/src/lidar_detection_node.cpp
@@ -14,9 +14,19 @@
 
 #include <chrono>
 #include <memory>
+#include <string>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+
+#include "cuda_runtime.h"
+
+#include "./params.h"
+#include "./pointpillar.h"
 
 using namespace std::chrono_literals;
 
@@ -27,7 +37,7 @@ class LidarDetectionNode : public rclcpp::Node
 {
 public:
   LidarDetectionNode()
-  : Node("lidar_detection_node"), count_(0)
+  : Node("lidar_object_detection"), count_(0)
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
@@ -39,17 +49,103 @@ private:
   {
     auto message = std_msgs::msg::String();
     message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    RCLCPP_INFO(this->get_logger(), "Publishing 2: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
+
+
 };
+  std::string home_path = "/home/docker/ament_ws/lidar_object_detection/";
+  std::string data_path = home_path + "data/";
+  std::string model_path = home_path + "model/pointpillar.onnx";
+
+
+void Getinfo(void)
+{
+  cudaDeviceProp prop;
+
+  int count = 0;
+  cudaGetDeviceCount(&count);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\nGPU has cuda devices: %d\n", count);
+  for (int i = 0; i < count; ++i) {
+    cudaGetDeviceProperties(&prop, i);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "----device id: %d info----\n", i);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  GPU : %s \n", prop.name);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  Capbility: %d.%d\n", prop.major, prop.minor);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  Global memory: %luMB\n", prop.totalGlobalMem >> 20);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  Const memory: %luKB\n", prop.totalConstMem  >> 10);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  SM in a block: %luKB\n", prop.sharedMemPerBlock >> 10);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  warp size: %d\n", prop.warpSize);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  threads in a block: %d\n", prop.maxThreadsPerBlock);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  block dim: (%d,%d,%d)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "  grid dim: (%d,%d,%d)\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+  }
+}
+
+
+int loadData(const char *file, void **data, unsigned int *length)
+{
+  std::fstream dataFile(file, std::ifstream::in);
+
+  if (!dataFile.is_open())
+  {
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Can't open files: "<< file);
+	  std::cout << "Can't open files: "<< file<<std::endl;
+	  return -1;
+  }
+
+  //get length of file:
+  unsigned int len = 0;
+  dataFile.seekg (0, dataFile.end);
+  len = dataFile.tellg();
+  dataFile.seekg (0, dataFile.beg);
+
+  //allocate memory:
+  char *buffer = new char[len];
+  if(buffer==NULL) {
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Can't malloc buffer."<<std::endl);
+	  std::cout << "Can't malloc buffer."<<std::endl;
+    dataFile.close();
+	  exit(-1);
+  }
+
+  //read data as a block:
+  dataFile.read(buffer, len);
+  dataFile.close();
+
+  *data = (void*)buffer;
+  *length = len;
+  return 0;  
+}
+
 
 int main(int argc, char * argv[])
 {
+  Getinfo();  // print GPU info
   rclcpp::init(argc, argv);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Initializing lidar node...");
+
+
+  cudaEvent_t start, stop;
+  float elapsedTime = 0.0f;
+  cudaStream_t stream = NULL;
+
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
+  checkCudaErrors(cudaStreamCreate(&stream));
+
+  Params params_;
+
+  std::vector<Bndbox> nms_pred;
+  nms_pred.reserve(100);
+
+  PointPillar pointpillar(model_path, stream);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Initialized pointpillar model...");
+
+
   rclcpp::spin(std::make_shared<LidarDetectionNode>());
   rclcpp::shutdown();
   return 0;
