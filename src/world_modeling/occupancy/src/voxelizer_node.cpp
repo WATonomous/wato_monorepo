275e5dda26683dff20aec130d88c3196ace2aa03
Owen Leather <owenleather03@gmail.com>
2023-06-10 04:30:25 +0000

    fixed pcl errors (working voxel grid)

--- src/world_modeling/occupancy/src/voxelizer_node.cpp
+++ src/world_modeling/occupancy/src/voxelizer_node.cpp
@@ -3,16 +3,17 @@
 
 #include "voxelizer_node.hpp"
 
-VoxelizerNode::VoxelizerNode() : Node("voxelizer"), voxilizer_(world_modeling::occupancy::Voxilizer()) {
+VoxelizerNode::VoxelizerNode() : Node("voxelizer") {
   RCLCPP_INFO(this->get_logger(), "Voxelizer Node\n");
 
-  pointcloud_sub_ = this->create_subscription<pcl::PCLPointCloud2>(
-    "PointCloud", ADVERTISING_FREQ,
+  pointcloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "LIDAR_TOP", ADVERTISING_FREQ,
     std::bind(
       &VoxelizerNode::voxel_sub_callback, this,
       std::placeholders::_1));
 
-  voxelgrid_pub_ = this->create_publisher<pcl::VoxelGrid>("VoxelGrid", ADVERTISING_FREQ);
+  voxelgrid_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("voxel_grid_test", ADVERTISING_FREQ);
+  marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("voxel_grid_marker", ADVERTISING_FREQ);
 
   // Define the default values for parameters if not defined in params.yaml
   this->declare_parameter("version", rclcpp::ParameterValue(0));
@@ -20,26 +21,53 @@ VoxelizerNode::VoxelizerNode() : Node("voxelizer"), voxilizer_(world_modeling::o
 }
 
 
-void VoxelizerNode::voxel_sub_callback(const pcl::PCLPointCloud2::Ptr msg)
+void VoxelizerNode::voxel_sub_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
-  // pcl::PCLPointCloud2::SharedPtr cloud = msg;
-  // pcl::PCLPointCloud2::Ptr voxel_cloud (new pcl::PCLPointCloud2());
-  pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2());
-  pcl::PCLPointCloud2::Ptr voxel_cloud (new pcl::PCLPointCloud2());
+  pcl::PCLPointCloud2::Ptr cloud2 (new pcl::PCLPointCloud2());
+  pcl_conversions::toPCL(*msg, *cloud2);
 
   pcl::VoxelGrid<pcl::PCLPointCloud2> voxel_filter;
-  voxel_filter.setInputCloud(cloud);
-  voxel_filter.setLeafSize(0.05,0.05,0.05);
-  voxel_filter.filter(*voxel_cloud);
+  voxel_filter.setInputCloud(cloud2);
+  voxel_filter.setLeafSize(voxel_size, voxel_size, voxel_size);
 
-  this->voxel_publish(voxel_filter);
+  pcl::PCLPointCloud2::Ptr cloud_filtered2 (new pcl::PCLPointCloud2());
+  voxel_filter.filter(*cloud_filtered2);
+
+  auto pub_msg = sensor_msgs::msg::PointCloud2();
+  pcl_conversions::fromPCL(*cloud_filtered2, pub_msg);
+
+  this->voxel_publish(pub_msg);
 }
 
-void VoxelizerNode::voxel_publish(const pcl::VoxelGrid<pcl::PCLPointCloud2>Ptr msg)
+void VoxelizerNode::voxel_publish(const sensor_msgs::msg::PointCloud2 voxel_grid)
 {
-  auto pub_msg = msg; //I should probably just pass the msg but I am just going to copy the sample node.
+  auto marker = visualization_msgs::msg::Marker();
+  
+  std_msgs::msg::Header header; 
+  header.stamp = rclcpp::Clock().now(); // time
+  header.frame_id = "LIDAR_TOP";
+
+  marker.header = header;
+  
+  marker.type = 6; // Cube List
+
+  marker.scale.x = voxel_size;
+  marker.scale.y = voxel_size;
+  marker.scale.z = voxel_size;
+  marker.frame_locked = true;
+
+  for (sensor_msgs::PointCloud2ConstIterator<float> it(voxel_grid, "x"); it != it.end(); ++it) {
+    auto point = geometry_msgs::msg::Point();
+    // re-quantize voxel grid by rounding to nearest voxel_size grid
+    point.x = int(it[0]/voxel_size)*voxel_size;
+    point.y = int(it[1]/voxel_size)*voxel_size;
+    point.z = int(it[2]/voxel_size)*voxel_size;
+    marker.points.push_back(point);
+  }
+
   RCLCPP_INFO(this->get_logger(), "Publishing Voxel Grid Message...\n");
-  voxelgrid_pub_->publish(pub_msg);
+  voxelgrid_pub_->publish(voxel_grid);
+  marker_pub_->publish(marker);
 }
 
 
