6ac878e62baf2a0086ddb38138ad12d633febc8c
Owen Leather <owenleather03@gmail.com>
2023-06-10 04:30:25 +0000

    fixed pcl errors (working voxel grid)

--- src/world_modeling/occupancy/include/voxelizer_node.hpp
+++ src/world_modeling/occupancy/include/voxelizer_node.hpp
@@ -4,7 +4,16 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/approximate_voxel_grid.h>
+#include <pcl/filters/uniform_sampling.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
 #include "world_modeling_msgs/msg/voxel_grid.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/point_cloud2_iterator.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "std_msgs/msg/header.hpp"
 
 
 class VoxelizerNode : public rclcpp::Node
@@ -14,13 +23,14 @@ class VoxelizerNode : public rclcpp::Node
     VoxelizerNode();
 
   private:
-  void VoxelizerNode::voxel_sub_callback(const pcl::PCLPointCloud2::Ptr msg);
+  void voxel_sub_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
-  void VoxelizerNode::voxel_publish(const pcl::VoxelGrid<pcl::PCLPointCloud2>Ptr msg);
+  void voxel_publish(const sensor_msgs::msg::PointCloud2 voxel_grid);
 
-  rclcpp::Subscription<pcl::PCLPointCloud2>::SharedPtr pointcloud_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_sub_;
 
-  rclcpp::Publisher<pcl::VoxelGrid>::SharedPtr voxelgrid_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr voxelgrid_pub_;
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
 
-  world_modeling::occupancy::Voxilizer voxilizer_;
+  float voxel_size = 0.1;
 };
