a2162b51ac6460e34e79aabd1709e09e6523a5bb
VishGit1234 <vishalschool1234@gmail.com>
2023-06-18 03:21:58 +0000

    Added sample ros node for getting carla data

---
+++ src/simulation/carla_sample_node/launch/carla_sample_node.launch.py
@@ -0,0 +1,8 @@
+from launch import LaunchDescription
+import launch_ros.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            namespace= "carla_sample_node", package='carla_sample_node', executable='carla_sample_node', output='screen'),
+    ])
