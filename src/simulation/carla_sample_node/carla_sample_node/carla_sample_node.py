e3ec032ed7b0758937bf45c340edb05177203811
VishGit1234 <vishalschool1234@gmail.com>
2023-06-19 20:35:58 +0000

    Added carla instance scaling for data logging

--- src/simulation/carla_sample_node/carla_sample_node/carla_sample_node.py
+++ src/simulation/carla_sample_node/carla_sample_node/carla_sample_node.py
@@ -4,19 +4,34 @@ from rclpy.node import Node
 from nav_msgs.msg import Odometry # For odometry
 from sensor_msgs.msg import NavSatFix # For GNSS
 
+from std_msgs.msg import Bool
+
+import os
+
 class Datalogger(Node):
     def __init__(self):
         super().__init__('datalogger')
-        self.subscription = self.create_subscription(
+        self.containerId = os.getenv('LOG_FILE', 'log')
+        self.gnssSubscription = self.create_subscription(
             NavSatFix,
             '/carla/ego_vehicle/gnss',
-            self.listener_callback,
+            self.gnss_callback,
             10
         )
-        self.subscription  # prevent unused variable warning
-    def listener_callback(self, msg):
-        with open("/home/docker/ament_ws/src/carla_sample_node/logs/gnss_log.txt", 'a+') as file:
+        self.gnssSubscription  # prevent unused variable warning
+        self.autopilotPublisher = self.create_publisher(
+            Bool,
+            '/carla/ego_vehicle/enable_autopilot',
+            10
+        )
+        self.timer = self.create_timer(10, self.timer_callback) # Publish autopilot message every 10 seconds
+    def gnss_callback(self, msg):
+        with open("/home/docker/ament_ws/src/carla_sample_node/logs/gnss_" + self.containerId + ".txt", 'a+') as file:
             file.write(str(msg.latitude) + ", " + str(msg.longitude) + "\n")
+    def timer_callback(self):
+        msg = Bool()
+        msg.data = True
+        self.autopilotPublisher.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
