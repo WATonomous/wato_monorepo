2bfec455952c1f9dd025af1d9572dee6a0958715
k-kaps <karan.kapoor@uwaterloo.ca>
2023-07-02 19:30:42 +0000

    router modified

--- src/world_modeling/hd_map/src/hd_map_router.cpp
+++ src/world_modeling/hd_map/src/hd_map_router.cpp
@@ -13,9 +13,29 @@ lanelet::GPSPoint ros_gps_msg_to_lanelet_gps_point(sensor_msgs::msg::NavSatFix::
 }
 
 bool HDMapRouter::set_lanelet(const lanelet::LaneletMapPtr &lanelet_ptr){
-    this->lanelet_ptr_ = lanelet_ptr;
     // TODO: Build traffic rules factory and assign routing graph
+    this->lanelet_ptr_ = lanelet_ptr;
+
+    /* 
+    **OPTIONAL**
+    //Routing Costs
+    double laneChangeCost = <add-lane-change-cost>;
+    RoutingCostPtrs costPtrs{std::make_shared<RoutingCostDistance>(laneChangeCost)};
+    //Config
+    RoutingGraph::Configuration routingGraphConf;
+    routingGraphConf.emplace(std::make_pair(RoutingGraph::ParticipantHeight, Attribute("2.")));
+    */
+    lanelet::traffic_rules::TrafficRulesPtr trafficRules{lanelet::traffic_rules::TrafficRulesFactory::instance().create(lanelet::Locations::Germany, lanelet::Participants::Vehicle)};
+    lanelet::routing::RoutingGraphPtr graph = lanelet::routing::RoutingGraph::build(*this->lanelet_ptr_, *trafficRules);
+
+    if (graph == nullptr){
+        RCLCPP_INFO(rclcpp::get_logger("hd_map_router"), "Routing Graph Build Failed...");
+        return false;
+    }
+
+    RCLCPP_INFO(rclcpp::get_logger("hd_map_router"), "Routing Graph Build Success...");
     return true;
+    
 }
 
 lanelet::ConstLanelet HDMapRouter::get_nearest_lanelet_to_gps(lanelet::GPSPoint gps_point){
@@ -29,6 +49,7 @@ lanelet::Optional<lanelet::routing::Route> HDMapRouter::route(lanelet::GPSPoint 
 lanelet::Optional<lanelet::routing::Route> HDMapRouter::route(lanelet::ConstLanelet from_lanelet, lanelet::ConstLanelet to_lanelet){
     //TODO: Implementation of route function to get shortest path from from_lanelet to to_lanelet
     //https://github.com/fzi-forschungszentrum-informatik/Lanelet2/blob/master/lanelet2_examples/src/06_routing/main.cpp
+
 }
 
 lanelet::LaneletMapPtr HDMapRouter::get_lanelet(){
