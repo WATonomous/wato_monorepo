119aca305f70739fb097755fffd27ac94b8f953c
Eddy Zhou <e23zhou@uwaterloo.ca>
2023-08-31 01:28:20 +0000

    refactored python code to fit 'core and node' paradigm

--- src/samples/python/producer/setup.py
+++ src/samples/python/producer/setup.py
@@ -27,7 +27,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'producer = producer.producer:main'
+            'producer_node = producer.producer_node:main'
         ],
     },
 )
