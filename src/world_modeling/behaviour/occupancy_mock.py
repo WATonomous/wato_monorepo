aeb9de99f685d711a41eed411bbe4410e3976eaa
Andrew Zhao <andrew2002zhao@gmail.com>
2023-09-12 19:32:40 +0000

    created behaviour  dockerfile and occupancy_mock.py file

---
+++ src/world_modeling/behaviour/occupancy_mock.py
@@ -0,0 +1,18 @@
+# import gymnasium as gym
+# from highway_env.envs.common.observation import ObservationType
+# from highway_env.vehicle.kinematics import Vehicle
+
+
+
+
+def main():
+    print("hello")
+
+# def mark_matrix_cells(vehicle):
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
