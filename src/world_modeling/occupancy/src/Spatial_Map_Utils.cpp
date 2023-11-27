14990d4ca578979953f9c4ffffde481f50157c15
hasan3773 <hasan.tahir@watonomous.ca>
2023-11-27 03:01:55 +0000

    Setting up spatial temporal map util function

---
+++ src/world_modeling/occupancy/src/Spatial_Map_Utils.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+#include "Spatial_Map_Utils.hpp"
+
+void add_vehicle_to_map(common_msgs::msg::Obstacle vehicle, nav_msgs::msg::OccupancyGrid::SharedPtr map){
+    // let him cook
+}
+
+
