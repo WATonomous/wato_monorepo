a77b5a420edeb3e0c8e61ece3d766784244aa7d5
Eddy Zhou <e23zhou@uwaterloo.ca>
2023-12-03 18:23:09 +0000

    lets try this instead

--- .github/templates/docker_context/docker_context.sh
+++ .github/templates/docker_context/docker_context.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Setup watod Environment
-# bash scripts/watod-setup-env.sh
-
 ################# Sweep for Docker Services and Profiles #################
 # Scans for services and modules in the wato_monorepo,
 # dynamically builds a json matrix for downstream CI build and testing
@@ -43,3 +40,6 @@ REPOSITORY=$(echo "$REGISTRY_URL" | sed 's|^.*/\(.*\)$|\1|')
 
 echo "registry=$REGISTRY" >> $GITHUB_OUTPUT
 echo "repository=$REPOSITORY" >> $GITHUB_OUTPUT
+
+################# Setup watod Environment #################
+bash scripts/watod-setup-env.sh
