51f65ded583fde1492ba378d2fefc6ee7901b846
Eddy Zhou <e23zhou@uwaterloo.ca>
2023-12-04 07:00:11 +0000

    ?

--- .github/templates/docker_context/docker_context.sh
+++ .github/templates/docker_context/docker_context.sh
@@ -31,8 +31,8 @@ while read -r module; do
 done <<< "$modules"
 
 # Convert the array of JSON objects to a single JSON array
-json_services=$(jq -s . <<< "${json_objects[*]}")
-echo docker_matrix=$json_services >> $GITHUB_OUTPUT
+json_services=$(jq -nc '$ARGS.positional' --args -- "${json_objects[*]}")
+echo "docker_matrix=$json_services" >> $GITHUB_OUTPUT
 
 ################# Setup Docker Registry and Repository Name #################
 # Docker Registry to pull/push images
@@ -41,5 +41,5 @@ REGISTRY_URL="ghcr.io/watonomous/wato_monorepo"
 REGISTRY=$(echo "$REGISTRY_URL" | sed 's|^\(.*\)/.*$|\1|')
 REPOSITORY=$(echo "$REGISTRY_URL" | sed 's|^.*/\(.*\)$|\1|')
 
-echo registry=$REGISTRY >> $GITHUB_OUTPUT
-echo repository=$REPOSITORY >> $GITHUB_OUTPUT
+echo "registry=$REGISTRY" >> $GITHUB_OUTPUT
+echo "repository=$REPOSITORY" >> $GITHUB_OUTPUT
