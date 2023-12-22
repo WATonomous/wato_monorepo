648b755f2eb5e844563aafe208371438fe592876
Eddy Zhou <eddy.zhou@rapyuta-robotics.com>
2023-12-22 17:35:43 +0900

    lets try the watod way

--- .github/templates/test/test_image.sh
+++ .github/templates/test/test_image.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Running $IMAGE tests..."
+bash scripts/watod-setup-env.sh
+
+echo "Running $SERVICE tests..."
+CONTAINER_NAME="$SERVICE-github-actions"
+
+# Run docker-compose service in detached mode and prevent early exit
+bash watod run -d --name "$CONTAINER_NAME" "$SERVICE" tail -f /dev/null
 
 # Run tests for ros2 packages, on failure script will exit with error message
-docker run --user="$FIXUID:$FIXGID" --rm "$IMAGE" /bin/bash -c "source ~/.bashrc; colcon test; colcon test-result --verbose"
+docker exec "$CONTAINER_NAME" /bin/bash -c "source ~/.bashrc; colcon test; colcon test-result --verbose"
+
+docker stop "$CONTAINER_NAME"
+docker rm "$CONTAINER_NAME"
