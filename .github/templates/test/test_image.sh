eee13ddeab3f46417f6286f041b517683674b5af
Eddy Zhou <eddy.zhou@rapyuta-robotics.com>
2023-12-22 17:20:29 +0900

    testing tests

--- .github/templates/test/test_image.sh
+++ .github/templates/test/test_image.sh
@@ -4,4 +4,4 @@ set -e
 echo "Running $IMAGE tests..."
 
 # Run tests for ros2 packages, on failure script will exit with error message
-docker run --rm "$IMAGE" /bin/bash -c "source ~/.bashrc; colcon test; colcon test-result --verbose"
+docker run --user="$FIXUID:$FIXGID" --rm "$IMAGE" /bin/bash -c "source ~/.bashrc; colcon test; colcon test-result --verbose"
