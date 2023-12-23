0c126f17c57032f537113e1ecbdcb3d7d6e12e59
Eddy Zhou <e23zhou@uwaterloo.ca>
2023-12-23 16:57:13 +0000

    pull

--- .github/templates/test/test_image.sh
+++ .github/templates/test/test_image.sh
@@ -7,6 +7,7 @@ echo "Running $SERVICE tests..."
 CONTAINER_NAME="$SERVICE-github-actions"
 
 # Run docker-compose service in detached mode and prevent early exit
+bash watod pull "$SERVICE"
 bash watod run -d --name "$CONTAINER_NAME" "$SERVICE" tail -f /dev/null
 
 # Run tests for ros2 packages, on failure script will exit with error message
