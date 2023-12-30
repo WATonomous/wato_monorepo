2f736d6e29d71a89cabbfe02249d6366e715a75a
Eddy Zhou <e23zhou@uwaterloo.ca>
2023-12-30 16:34:42 +0000

    dude wtf

--- .github/templates/test/test_image.sh
+++ .github/templates/test/test_image.sh
@@ -6,10 +6,9 @@ CONTAINER_NAME="$SERVICE-github-actions"
 
 # Run docker-compose service in detached mode and prevent early exit
 bash watod -dev pull "$SERVICE"
-bash watod run -d --name "$CONTAINER_NAME" "$SERVICE" tail -f /dev/null
+bash watod -dev run -d --name "$CONTAINER_NAME" "$SERVICE" tail -f /dev/null
 
 # Run tests for ros2 packages, on failure script will exit with error message
 docker exec "$CONTAINER_NAME" /bin/bash -c "source /home/bolty/ament_ws/install/setup.bash; colcon test; colcon test-result --verbose"
 
-docker stop "$CONTAINER_NAME"
-docker rm "$CONTAINER_NAME"
+bash watod -dev down
