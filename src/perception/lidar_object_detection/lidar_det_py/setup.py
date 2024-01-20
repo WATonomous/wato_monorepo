acc1711bf84ddebf9cdedd161867b829d4f6acfb
Danryanh7 <danielryanh7@gmail.com>
2024-01-20 06:12:35 +0000

    added python package if I want to use that

---
+++ src/perception/lidar_object_detection/lidar_det_py/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'lidar_det_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bolty',
+    maintainer_email='bolty@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'test_node = lidar_det_py.test_node:main'
+        ],
+    },
+)
