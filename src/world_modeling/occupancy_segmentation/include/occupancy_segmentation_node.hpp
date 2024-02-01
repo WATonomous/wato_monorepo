c81ee89ffd5eb6a9a7797cb38c32c5be2e37faeb
jmogen <jmogen@uwaterloo.ca>
2024-02-01 13:35:48 +0000

    cleaned up extra bits, fumbling around with ros LOL

--- src/world_modeling/occupancy_segmentation/include/occupancy_segmentation_node.hpp
+++ src/world_modeling/occupancy_segmentation/include/occupancy_segmentation_node.hpp
@@ -7,6 +7,8 @@
 #include "rcl_interfaces/msg/set_parameters_result.hpp"
 
 #include "sample_msgs/msg/unfiltered.hpp"
+#include "std_msgs/msg/int32.hpp"
+
 
 #include "occupancy_segmentation_core.hpp"
 
@@ -35,26 +37,12 @@ private:
    */
   void timer_callback();
 
-  /**
-   * Callback used to dynamically update velocity data at runtime.
-   *
-   * @param parameters list of parameters (only velocity in this case) that were modified
-   * @returns status message indicating whether update was successful
-   */
-  // rcl_interfaces::msg::SetParametersResult parameters_callback(
-  //   const std::vector<rclcpp::Parameter> & parameters);
-
   // ROS2 publisher sending raw messages to the unfiltered topic.
-  rclcpp::Publisher<sample_msgs::msg::Unfiltered>::SharedPtr data_pub_;
+  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr data_pub_;
 
   // ROS2 timer used to call data generation callback at fixed intervals.
   rclcpp::TimerBase::SharedPtr timer_;
 
-  // Callback to dynamically modify node parameters.
-  // rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_cb_;
-
-  // occupancy_segmentation implementation containing logic for coordinate serialization and management.
-  // world_modeling::OccupancySegmentationCore occupancy_segmentation_;
 };
 
 #endif  // PRODUCER_NODE_HPP_
--- src/world_modeling/occupancy_segmentation/src/occupancy_segmentation_node.cpp
+++ src/world_modeling/occupancy_segmentation/src/occupancy_segmentation_node.cpp
@@ -3,64 +3,32 @@
 #include <vector>
 
 #include "occupancy_segmentation_node.hpp"
+// #include "std_msgs/msg/int32.hpp"
 
 OccupancySegmentationNode::OccupancySegmentationNode(int delay_ms)
 : Node("occupancy_segmentation")
 {
   data_pub_ =
-    this->create_publisher<sample_msgs::msg::Unfiltered>("/unfiltered_topic", ADVERTISING_FREQ);
+    this->create_publisher<std_msgs::msg::Int32>("/jack_test_topic", ADVERTISING_FREQ);
 
   timer_ = this->create_wall_timer(
     std::chrono::milliseconds(delay_ms),
     std::bind(&OccupancySegmentationNode::timer_callback, this));
-
-  // Define the default values for parameters if not defined in params.yaml
-  // this->declare_parameter("pos_x", 0.0);
-  // this->declare_parameter("pos_y", 0.0);
-  // this->declare_parameter("pos_z", 0.0);
-  // this->declare_parameter("velocity", 0.0);
-
-  // rclcpp::Parameter pos_x = this->get_parameter("pos_x");
-  // rclcpp::Parameter pos_y = this->get_parameter("pos_y");
-  // rclcpp::Parameter pos_z = this->get_parameter("pos_z");
-  // rclcpp::Parameter velocity = this->get_parameter("velocity");
-
-  // occupancy_segmentation_.update_position(pos_x.as_double(), pos_y.as_double(), pos_z.as_double());
-  // occupancy_segmentation_.update_velocity(velocity.as_double());
-
-  // param_cb_ = this->add_on_set_parameters_callback(
-  //   std::bind(&OccupancySegmentationNode::parameters_callback, this, std::placeholders::_1));
 }
 
 void OccupancySegmentationNode::timer_callback()
 {
   // occupancy_segmentation_.update_coordinates();
-  auto msg = sample_msgs::msg::Unfiltered();
-  msg.timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(
-    std::chrono::system_clock::now().time_since_epoch()).count();
+  auto msg = std_msgs::msg::Int32();
+  msg.data = 1;
+  // msg.timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(
+  //   std::chrono::system_clock::now().time_since_epoch()).count();
   // occupancy_segmentation_.serialize_coordinates(msg);
 
-  RCLCPP_INFO(this->get_logger(), "Publishing: %llu", msg.timestamp);
+  RCLCPP_INFO(this->get_logger(), "Publishing: %llu", msg.data);
   data_pub_->publish(msg);
 }
 
-// rcl_interfaces::msg::SetParametersResult OccupancySegmentationNode::parameters_callback(const std::vector<rclcpp::Parameter> & parameters)
-// {
-//   rcl_interfaces::msg::SetParametersResult result;
-//   result.successful = false;
-//   result.reason = "";
-
-//   for (const auto & parameter : parameters) {
-//     if (parameter.get_name() == "velocity" &&
-//       parameter.get_type() == rclcpp::ParameterType::PARAMETER_INTEGER)
-//     {
-//       occupancy_segmentation_.update_velocity(parameter.as_int());
-//       RCLCPP_INFO(this->get_logger(), "Velocity successfully set to %d", parameter.as_int());
-//       result.successful = true;
-//     }
-//   }
-//   return result;
-// }
 
 int main(int argc, char ** argv)
 {
