665479d0a3cea51df8b2021ae470dc71e7bfff49
Steven Gong <gong.steven@hotmail.com>
2024-02-04 00:15:48 -0500

    Remove unnecessary import (#57)

--- src/perception/camera_object_detection/camera_object_detection/yolov8_detection.py
+++ src/perception/camera_object_detection/camera_object_detection/yolov8_detection.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
 from sensor_msgs.msg import Image, CompressedImage
 from vision_msgs.msg import ObjectHypothesisWithPose, Detection2D, Detection2DArray
 
