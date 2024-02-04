73401bbccc346dcd0cbe8c9959c81f09f2f6e1e1
Eddy Zhou <edsteredward@gmail.com>
2024-02-04 11:12:58 +0900

    test failure

--- .github/templates/test/test_image.sh
+++ .github/templates/test/test_image.sh
@@ -3,15 +3,3 @@ set -e
 
 docker pull -q $IMAGE:$TAG
 docker run $IMAGE:$TAG /bin/bash -c "source /home/bolty/ament_ws/install/setup.bash; colcon test; colcon test-result --verbose"
-
-# echo "Running $SERVICE tests..."
-# CONTAINER_NAME="$SERVICE-github-actions"
-
-# # Run docker-compose service with watod in detached mode and prevent early exit
-# bash watod -dev pull "$SERVICE"
-# bash watod -dev run -d --name "$CONTAINER_NAME" "$SERVICE" tail -f /dev/null
-
-# # Run tests for ros2 packages, on failure script will exit with error message
-# docker exec "$CONTAINER_NAME" /bin/bash -c "source /home/bolty/ament_ws/install/setup.bash; colcon test; colcon test-result --verbose"
-
-# bash watod -dev down
