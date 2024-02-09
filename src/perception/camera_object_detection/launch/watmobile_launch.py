b5f8ab826b5cb5aba39f90c616f05cfbdc772238
Steven Gong <s36gong@uwaterloo.ca>
2024-02-09 02:44:23 +0000

    Update launch file for 3 cameras

---
+++ src/perception/camera_object_detection/launch/watmobile_launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    config = os.path.join(
+        get_package_share_directory('camera_object_detection'),
+        'config',
+        'watmobile_config.yaml'
+    )
+
+    # nodes
+    left_camera_object_detection_node = Node(
+        package='camera_object_detection',
+        executable='camera_object_detection_node',
+        name='left_camera_object_detection_node',
+        parameters=[config]
+    )
+    
+    center_camera_object_detection_node = Node(
+        package='camera_object_detection',
+        executable='camera_object_detection_node',
+        name='center_camera_object_detection_node',
+        parameters=[config]
+    )
+
+    right_camera_object_detection_node = Node(
+        package='camera_object_detection',
+        executable='camera_object_detection_node',
+        name='right_camera_object_detection_node',
+        parameters=[config]
+    )
+
+    # finalize
+    ld.add_action(left_camera_object_detection_node)
+    ld.add_action(center_camera_object_detection_node)
+    ld.add_action(right_camera_object_detection_node)
+
+    return ld
