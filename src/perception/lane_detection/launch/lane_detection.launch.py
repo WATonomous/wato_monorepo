cac12e7101447a4a8bbea8722043a17ca93f076a
Justin Leung <justin.leung.ch@gmail.com>
2024-02-13 09:47:26 +0000

    Add launch and config files

--- src/perception/lane_detection/launch/lane_detection.launch.py
+++ src/perception/lane_detection/launch/lane_detection.launch.py
@@ -1,12 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('lane_detection'),
+        'config',
+        'config.yaml'
+    )
 
     return LaunchDescription([
         Node(
             package='lane_detection',
             executable='lane_detection',
             name='lane_detection',
+            parameters=[config]
         )
     ])
