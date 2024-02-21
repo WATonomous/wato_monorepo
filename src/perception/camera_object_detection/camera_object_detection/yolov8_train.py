addeba75b6ab4d0a7f95da516a600900fb43965a
Steven Gong <s36gong@uwaterloo.ca>
2024-02-21 06:00:12 +0000

    Add freeze option

--- src/perception/camera_object_detection/camera_object_detection/yolov8_train.py
+++ src/perception/camera_object_detection/camera_object_detection/yolov8_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 yolo = YOLO('yolov8m.pt')
-yolo.train(data='/perception_datasets/roboflow/traffic_light_roboflow_v0/data.yaml', epochs=300)
+yolo.train(data='/perception_datasets/roboflow/traffic_light_roboflow_v3/data.yaml', epochs=300, freeze=10)
 valid_results = yolo.val()
 print(valid_results)
