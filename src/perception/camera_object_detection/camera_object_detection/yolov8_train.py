14ad488459e028497acb7063dfe3509a86988422
Steven Gong <s36gong@uwaterloo.ca>
2024-02-21 02:44:26 +0000

    Update to medium size

--- src/perception/camera_object_detection/camera_object_detection/yolov8_train.py
+++ src/perception/camera_object_detection/camera_object_detection/yolov8_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-yolo = YOLO('yolov8s.pt')
+yolo = YOLO('yolov8m.pt')
 yolo.train(data='/perception_datasets/roboflow/traffic_light_roboflow_v0/data.yaml', epochs=300)
 valid_results = yolo.val()
 print(valid_results)
