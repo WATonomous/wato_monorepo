3811b6fb96c79e734cfd01b6051f1e727e8ae187
Jessica Ding <jessica2003mail@gmail.com>
2024-02-22 03:21:40 +0000

    autopep8 linting fixes

--- src/perception/camera_object_detection/test/camera_object_detection_test.py
+++ src/perception/camera_object_detection/test/camera_object_detection_test.py
@@ -10,12 +10,14 @@ import pytest
 from sensor_msgs.msg import Image
 from camera_object_detection.yolov8_detection import CameraDetectionNode
 
+
 @pytest.mark.launch_test
 def generate_test_description():
     # Get launch file and set up for starting the test
     pkg_share = get_package_share_directory('camera_object_detection')
     launch_dir = os.path.join(pkg_share, 'launch')
-    camera_detection_launch = os.path.join(launch_dir, 'camera_object_detection_test.launch.py')
+    camera_detection_launch = os.path.join(
+        launch_dir, 'camera_object_detection_test.launch.py')
 
     return launch.LaunchDescription([
         ExecuteProcess(
@@ -32,7 +34,7 @@ class TestCameraObjectDetectionLaunch(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         rclpy.init()
-        cls.node = CameraDetectionNode() # create a CameraDetectionNode to test
+        cls.node = CameraDetectionNode()  # create a CameraDetectionNode to test
         cls.logs_received = []
         cls.images_received = []
 
@@ -78,22 +80,28 @@ class TestCameraObjectDetectionLaunch(unittest.TestCase):
 
         # Test the image_callback function of the CameraDetectionNode node
         self.node.image_callback(dummy_image)
-        
+
         # Spin some time to collect logs
         end_time = self.node.get_clock().now() + rclpy.time.Duration(seconds=10)
         while rclpy.ok() and self.node.get_clock().now() < end_time:
             rclpy.spin_once(self.node, timeout_sec=0.1)
 
         # Check if the expected log messages were received
-        expected_log_messages = ["Successfully created node listening on camera topic:", "Finished in:"] # From CameraDetectionNode logger
+        # From CameraDetectionNode logger
+        expected_log_messages = [
+            "Successfully created node listening on camera topic:", "Finished in:"]
         for expected_msg in expected_log_messages:
             message_logged = any(
                 expected_msg in log.msg and log.name == "camera_object_detection_node"
                 for log in self.logs_received
             )
-            self.assertTrue(message_logged, f"Expected log message '{expected_msg}' not found in the logs.")
+            self.assertTrue(
+                message_logged, f"Expected log message '{expected_msg}' not found in the logs.")
 
-        self.assertTrue(len(self.images_received) > 0, "No visualization images were published.") # Check that given an image, the node is publishing
+        # Check that given an image, the node is publishing
+        self.assertTrue(len(self.images_received) > 0,
+                        "No visualization images were published.")
+
 
 if __name__ == '__main__':
     unittest.main()
