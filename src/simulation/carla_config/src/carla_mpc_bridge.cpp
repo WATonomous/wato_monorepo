0bb5563f89b3de8cd759ecdfc84a6dbb0126d9a6
WATonomousAdmin <admin@watonomous.ca>
2024-03-04 11:58:45 +0000

    Fix code style issues with clang_format

--- src/simulation/carla_config/src/carla_mpc_bridge.cpp
+++ src/simulation/carla_config/src/carla_mpc_bridge.cpp
@@ -11,76 +11,63 @@ class MPC_Bridge_Node : public rclcpp::Node {
   double xOff = 0;
   double yOff = 0;
 
-  rclcpp::Publisher<embedded_msgs::msg::SteeringAngleCAN>::SharedPtr
-      steeringPub;
-  rclcpp::Publisher<path_planning_msgs::msg::AckermannDrive>::SharedPtr
-      carlaPub;
+  rclcpp::Publisher<embedded_msgs::msg::SteeringAngleCAN>::SharedPtr steeringPub;
+  rclcpp::Publisher<path_planning_msgs::msg::AckermannDrive>::SharedPtr carlaPub;
 
-public:
+ public:
   MPC_Bridge_Node() : Node("carla_mpc_bridge") {
     // Declare Parameters
     this->declare_parameter("mpc_output_topic", "/mpc_output");
     this->declare_parameter("steering_publisher_topic", "/steering_data");
 
     // Get Parameters
-    std::string mpc_output_topic =
-        this->get_parameter("mpc_output_topic").as_string();
+    std::string mpc_output_topic = this->get_parameter("mpc_output_topic").as_string();
     std::string steering_publisher_topic =
         this->get_parameter("steering_publisher_topic").as_string();
 
     // Wait for service
-    auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(
-        this, "set_initial_pose");
+    auto parameters_client =
+        std::make_shared<rclcpp::SyncParametersClient>(this, "set_initial_pose");
     while (!parameters_client->wait_for_service(std::chrono::seconds(1))) {
       if (!rclcpp::ok()) {
-        RCLCPP_ERROR(this->get_logger(),
-                     "Interrupted while waiting for the service. Exiting.");
+        RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
         return;
       }
-      RCLCPP_INFO(this->get_logger(),
-                  "service not available, waiting again...");
+      RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
     }
 
     // Check if role name specified
     std::string role_name;
     std::string param_name = "role_name";
     do {
-      role_name = parameters_client->get_parameters({"role_name"})[0]
-                      .get_value<std::string>();
+      role_name = parameters_client->get_parameters({"role_name"})[0].get_value<std::string>();
       std::this_thread::sleep_for(std::chrono::seconds(1));
     } while (role_name == "");
 
     // Subscribe to vehicle status
-    rclcpp::Subscription<path_planning_msgs::msg::CarlaEgoVehicleStatus>::
-        SharedPtr steeringAngleSub;
-    steeringAngleSub = this->create_subscription<
-        path_planning_msgs::msg::CarlaEgoVehicleStatus>(
+    rclcpp::Subscription<path_planning_msgs::msg::CarlaEgoVehicleStatus>::SharedPtr
+        steeringAngleSub;
+    steeringAngleSub = this->create_subscription<path_planning_msgs::msg::CarlaEgoVehicleStatus>(
         "/carla/" + role_name + "/vehicle_status", 2,
         std::bind(&MPC_Bridge_Node::publish_steering, this, _1));
 
     // Subscribe to MPC Output
     rclcpp::Subscription<path_planning_msgs::msg::MPCOutput>::SharedPtr mpcSub;
     mpcSub = this->create_subscription<path_planning_msgs::msg::MPCOutput>(
-        mpc_output_topic, 2,
-        std::bind(&MPC_Bridge_Node::mpc_to_carla, this, _1));
+        mpc_output_topic, 2, std::bind(&MPC_Bridge_Node::mpc_to_carla, this, _1));
 
-    rclcpp::Subscription<path_planning_msgs::msg::Environment>::SharedPtr
-        envSub;
+    rclcpp::Subscription<path_planning_msgs::msg::Environment>::SharedPtr envSub;
     envSub = this->create_subscription<path_planning_msgs::msg::Environment>(
-        "/path_planning/environment", 2,
-        std::bind(&MPC_Bridge_Node::env_callback, this, _1));
+        "/path_planning/environment", 2, std::bind(&MPC_Bridge_Node::env_callback, this, _1));
 
     this->steeringPub =
-        this->create_publisher<embedded_msgs::msg::SteeringAngleCAN>(
-            steering_publisher_topic, 1);
-    this->carlaPub =
-        this->create_publisher<path_planning_msgs::msg::AckermannDrive>(
-            "/carla/" + role_name + "/ackermann_cmd", 1);
+        this->create_publisher<embedded_msgs::msg::SteeringAngleCAN>(steering_publisher_topic, 1);
+    this->carlaPub = this->create_publisher<path_planning_msgs::msg::AckermannDrive>(
+        "/carla/" + role_name + "/ackermann_cmd", 1);
   }
 
-private:
-  void publish_steering(
-      path_planning_msgs::msg::CarlaEgoVehicleStatus::SharedPtr vehicle) {
+ private:
+  void publish_steering(path_planning_msgs::msg::CarlaEgoVehicleStatus::SharedPtr vehicle) {
     // steer is between -1 and 1, max steer is 70 degrees (1.22173rad), so
     // multiply steer by rad to get approximate steer rwa =
     // (steer.steering_angle * 0.0629 - 0.1363) * 0.0174533;
