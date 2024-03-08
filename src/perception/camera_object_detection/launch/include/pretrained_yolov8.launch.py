aa068790b4503f7459b9355b51868a9f6fb2fc12
Steven Gong <s36gong@uwaterloo.ca>
2024-03-08 01:58:02 +0000

    Clean up launch files

--- src/perception/camera_object_detection/launch/include/pretrained_yolov8.launch.py
+++ src/perception/camera_object_detection/launch/include/pretrained_yolov8.launch.py
@@ -5,14 +5,12 @@ import os
 
 
 def generate_launch_description():
-    ld = LaunchDescription()
     config = os.path.join(
         get_package_share_directory('camera_object_detection'),
         'config',
         'eve_config.yaml'
     )
 
-    # nodes
     left_camera_object_detection_node = Node(
         package='camera_object_detection',
         executable='camera_object_detection_node',
@@ -33,10 +31,9 @@ def generate_launch_description():
         name='right_camera_object_detection_node',
         parameters=[config]
     )
-
-    # finalize
-    ld.add_action(left_camera_object_detection_node)
-    ld.add_action(center_camera_object_detection_node)
-    ld.add_action(right_camera_object_detection_node)
-
-    return ld
+    
+    return LaunchDescription([
+        left_camera_object_detection_node,
+        center_camera_object_detection_node,
+        right_camera_object_detection_node,
+    ])
