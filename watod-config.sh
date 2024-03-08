32ea4388238f74a3fa34a9deaf81ed96efd93c1c
Steven Gong <s36gong@uwaterloo.ca>
2024-03-08 04:41:10 +0000

    Clean up watod-config.sh

--- watod-config.sh
+++ watod-config.sh
@@ -15,7 +15,7 @@
 ##	 - simulation			:	starts simulation
 ##   - samples             	:   starts sample ROS2 pubsub nodes
 
-ACTIVE_MODULES="perception infrastructure"
+# ACTIVE_MODULES=""
 
 ################################# MODE OF OPERATION #################################
 ## Possible modes of operation when running watod.
@@ -27,7 +27,7 @@ ACTIVE_MODULES="perception infrastructure"
 
 ############################## ADVANCED CONFIGURATIONS ##############################
 ## Name to append to docker containers. DEFAULT = "<your_watcloud_username>"
-COMPOSE_PROJECT_NAME="j89leung"
+# COMPOSE_PROJECT_NAME=""
 
 ## Tag to use. Images are formatted as <IMAGE_NAME>:<TAG> with forward slashes replaced with dashes.
 ## DEFAULT = "<your_current_github_branch>"
