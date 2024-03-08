56d5128c886d08507ef21cc1ab576cacb2227412
Mark Chiu <miniapple8888@gmail.com>
2024-03-07 23:54:56 -0500

    Add traffic sign launch file (#83)
    * initial commit
    
    * Revert unecessary changes
    
    * Remove model data
    
    * Add CenterCrop and modify training parameters
    
    * Remove build and install folder
    
    * Uncommit watod-config
    
    * Change traffic signs config
    
    * Uncommit changes
    
    * Uncommit changes launch files
    
    * Remove train + restore detection
    
    * Fix lint errors
    
    * Fix lint errors 2
    
    * Remove remnant of perception traffic signs node
    
    * Update launch file
    
    * Restore to medium size
    
    * Remove redundant code
    
    * Add back line
    
    ---------
    
    Co-authored-by: Steven Gong <s36gong@uwaterloo.ca>

--- src/perception/camera_object_detection/launch/eve.launch.py
+++ src/perception/camera_object_detection/launch/eve.launch.py
@@ -14,8 +14,14 @@ def generate_launch_description():
         default_value=launch_traffic_light,
         description="Launch traffic light detection",
     )
+    launch_traffic_signs = LaunchConfiguration("launch_traffic_signs", default=True)
+    launch_traffic_signs_arg = DeclareLaunchArgument(
+        "launch_traffic_signs",
+        default_value=launch_traffic_signs,
+        description="Launch traffic signs detection",
+    )
 
-    launch_args = [launch_traffic_light_arg]
+    launch_args = [launch_traffic_light_arg, launch_traffic_signs_arg]
 
     camera_object_detection_launch_include_dir = os.path.join(
         get_package_share_directory("camera_object_detection"), "launch", "include"
@@ -34,10 +40,18 @@ def generate_launch_description():
         condition=LaunchConfigurationEquals("launch_traffic_light", "True"),
     )
 
+    traffic_signs_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [camera_object_detection_launch_include_dir, "/traffic_signs.launch.py"]
+        ),
+        condition=LaunchConfigurationEquals("launch_traffic_signs", "True"),
+    )
+
     return LaunchDescription(
         launch_args
         + [
             pretrained_yolov8_launch,
             traffic_light_launch,
+            traffic_signs_launch,
         ]
     )
