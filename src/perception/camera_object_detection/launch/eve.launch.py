dff7f45d79b4f01113b29d488cce0895b9510f32
Steven Gong <s36gong@uwaterloo.ca>
2024-03-08 02:18:27 +0000

    Update file names

--- src/perception/camera_object_detection/launch/eve.launch.py
+++ src/perception/camera_object_detection/launch/eve.launch.py
@@ -12,16 +12,11 @@ def generate_launch_description():
     launch_traffic_light_arg = DeclareLaunchArgument('launch_traffic_light', 
                                                     default_value=launch_traffic_light,
                                                     description='Launch traffic light detection')
-    launch_traffic_sign = LaunchConfiguration('launch_traffic_sign', default=True)
-    launch_traffic_sign_arg = DeclareLaunchArgument('launch_traffic_sign', 
-                                                    default_value=launch_traffic_sign,
-                                                    description='Launch traffic sign detection')
     
-    launch_args = [launch_traffic_light_arg,
-                   launch_traffic_sign_arg]
+    launch_args = [launch_traffic_light_arg]
 
     camera_object_detection_launch_include_dir = os.path.join(
-        get_package_share_directory('carter_navigation'), 'launch', 'include')
+        get_package_share_directory('camera_object_detection'), 'launch', 'include')
 
     pretrained_yolov8_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -35,15 +30,7 @@ def generate_launch_description():
             'launch_traffic_light', 'True')
     )
 
-    traffic_sign_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [camera_object_detection_launch_include_dir, '/traffic_sign.launch.py']),
-        condition=LaunchConfigurationEquals(
-            'launch_traffic_sign', 'True')
-    )
-
     return LaunchDescription(launch_args + [
         pretrained_yolov8_launch,
         traffic_light_launch,
-        traffic_sign_launch
     ])
