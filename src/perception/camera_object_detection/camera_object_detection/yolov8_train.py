722e21b2ffbf69ee1f6917df423f1aeff5948a40
Albert <albert.nguyen.tran1@gmail.com>
2024-03-08 04:38:24 +0000

    train on new data set

--- src/perception/camera_object_detection/camera_object_detection/yolov8_train.py
+++ src/perception/camera_object_detection/camera_object_detection/yolov8_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 yolo = YOLO('yolov8m.pt')
-yolo.train(data='/perception_datasets/roboflow/traffic_light_roboflow_v3/data.yaml', epochs=300, freeze=10)
+yolo.train(data='/perception_datasets/roboflow/bigger_traffic_light_roboflow_v1/data.yaml', epochs=300, freeze=10)
 valid_results = yolo.val()
 print(valid_results)
