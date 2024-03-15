2f02e852cc43b352ea4af7fa4d703f4104387e57
Justin Leung <justin.leung.ch@gmail.com>
2024-03-15 06:15:18 +0000

    Address PR comments

--- src/perception/lane_detection/launch/nuscenes.launch.py
+++ src/perception/lane_detection/launch/nuscenes.launch.py
@@ -3,19 +3,23 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-
 def generate_launch_description():
+    ld = LaunchDescription()
+
     config = os.path.join(
-        get_package_share_directory("lane_detection"), "config", "nuscenes_config.yaml"
+        get_package_share_directory('lane_detection'),
+        'config',
+        'nuscenes_config.yaml'
     )
 
-    return LaunchDescription(
-        [
-            Node(
-                package="lane_detection",
-                executable="lane_detection",
-                name="lane_detection_node",
-                parameters=[config],
-            )
-        ]
+    lane_detection_node = Node(
+        package='lane_detection',
+        executable='lane_detection',
+        name='lane_detection_node',
+        parameters=[config],
+        arguments=['--ros-args', '--log-level', 'info']
     )
+
+    ld.add_action(lane_detection_node)
+
+    return ld
