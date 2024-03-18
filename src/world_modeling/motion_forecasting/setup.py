b44e85d3ed4aed1b047a1e553e359783581e38ed
Dygitz <dom_ritz@icloud.com>
2024-03-18 01:03:10 +0000

    Created setup.py, motion_forecasting.launch.py and motion_forecasting_config.yaml for motion_forecasting

---
+++ src/world_modeling/motion_forecasting/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'motion_forecasting'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Dominik',
+    maintainer_email='dom_ritz@watonomous.ca',
+    description='A ROS 2 package for motion forecasting in autonomous systems.',
+    license='Specify your license',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'motion_forecasting_node = motion_forecasting_node:main'
+        ],
+    },
+)
