1fee2af25f4c2f57cb83687b2f46674c4f009a61
Danryanh7 <danielryanh7@gmail.com>
2024-04-04 23:23:38 +0000

    fixing styling

--- src/perception/lidar_object_detection/lidar_object_detection/launch/eve_launch.py
+++ src/perception/lidar_object_detection/lidar_object_detection/launch/eve_launch.py
@@ -3,6 +3,7 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
     ld = LaunchDescription()
     config = os.path.join(
