3a9fec6c5e92658126e16eedf7c26e1ce445d08e
Momin Chaudhry <hellomomins@yahoo.com>
2024-06-03 21:37:36 +0000

    working on combined

---
+++ src/perception/camera_object_detection/launch/include/combined_model.launch.py
@@ -0,0 +1,40 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory("camera_object_detection"), 
+        "config", 
+        "combined_config.yaml"
+    )
+
+    left_combined_detection_node = Node(
+        package="camera_object_detection",
+        executable="camera_object_detection_node",
+        name="left_combined_detection_node",
+        parameters=[config],
+    )
+
+    center_combined_detection_node = Node(
+        package="camera_object_detection",
+        executable="camera_object_detection_node",
+        name="center_combined_detection_node",
+        parameters=[config],
+    )
+
+    right_combined_detection_node = Node(
+        package="camera_object_detection",
+        executable="camera_object_detection_node",
+        name="right_combined_detection_node",
+        parameters=[config],
+    )
+
+    return LaunchDescription(
+        [
+            left_combined_detection_node,
+            center_combined_detection_node,
+            right_combined_detection_node,
+        ]
+    )
