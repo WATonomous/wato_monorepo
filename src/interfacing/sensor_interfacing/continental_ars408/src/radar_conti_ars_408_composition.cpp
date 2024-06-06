ccb7ff1bf25f29da9789eaed1efb290d51b41b0b
Meshva Desai <meshva03@gmail.com>
2024-06-06 14:18:13 +0000

    add more data fields

---
+++ src/interfacing/sensor_interfacing/continental_ars408/src/radar_conti_ars_408_composition.cpp
@@ -0,0 +1,34 @@
+// Copyright [2020] [Daniel Peter, peter@fh-aachen.de, Fachhochschule Aachen]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#include <memory>
+
+#include "../include/radar_conti_ars_408_component.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+
+    // Create an executor that will be responsible for execution of callbacks for a set of nodes.
+    // With this version, all callbacks will be called from within this thread (the main one).
+    rclcpp::executors::SingleThreadedExecutor exec;
+
+    rclcpp::NodeOptions options;
+    auto radar_conti_ars408_node = std::make_shared<FHAC::radar_conti_ars408>(options);
+    
+    exec.add_node(radar_conti_ars408_node->get_node_base_interface());
+    exec.spin();
+
+    rclcpp::shutdown();
+}
