58c8e3f14aa0c1af3251e44ec968eb3712be75f1
Lucas Reljic <lucas.reljic@gmail.com>
2024-06-07 03:37:11 +0000

    Fixed up node, downloaded model and fixed dockerfile

--- src/perception/semantic_segmentation/setup.py
+++ src/perception/semantic_segmentation/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/resource/model', glob('resource/model/*')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
