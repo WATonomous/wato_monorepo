417ae6ce2c0bda58860c31235c08c9c0987d21d1
Meshva Desai <meshva03@gmail.com>
2024-06-09 23:01:29 +0000

    linter

--- src/perception/tracking/dets_2d_3d/include/cluster.hpp
+++ src/perception/tracking/dets_2d_3d/include/cluster.hpp
@@ -1,34 +1,33 @@
 #pragma once
 
-#include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 #include <vision_msgs/msg/detection3_d_array.hpp>
 
 #include <vector>
 
-class Cluster
-{
+class Cluster {
 public:
-    // makes a cloud from all points `in_cloud_ptr` that are indexed by `in_cluster_indices`
-    Cluster(
-        const pcl::PointCloud<pcl::PointXYZ>::Ptr& in_cloud_ptr, 
-        const std::vector<int>& in_cluster_indices);
+  // makes a cloud from all points `in_cloud_ptr` that are indexed by
+  // `in_cluster_indices`
+  Cluster(const pcl::PointCloud<pcl::PointXYZ>::Ptr &in_cloud_ptr,
+          const std::vector<int> &in_cluster_indices);
 
-    pcl::PointXYZ getCentroid() { return centroid_; }
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr getCloud() { return cloud_; }
-    vision_msgs::msg::BoundingBox3D getBoundingBox();
+  pcl::PointXYZ getCentroid() { return centroid_; }
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr getCloud() { return cloud_; }
+  vision_msgs::msg::BoundingBox3D getBoundingBox();
 
-    bool isValid();
-    int size() { return cloud_->size(); }
+  bool isValid();
+  int size() { return cloud_->size(); }
 
 private:
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_;
-    pcl::PointXYZ centroid_;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_;
+  pcl::PointXYZ centroid_;
 
-    pcl::PointXYZ max_point_;
-    pcl::PointXYZ min_point_;
+  pcl::PointXYZ max_point_;
+  pcl::PointXYZ min_point_;
 
-    static int color_index;
-    static std::vector<std::vector<int>> colors;
+  static int color_index;
+  static std::vector<std::vector<int>> colors;
 };
--- src/perception/tracking/dets_2d_3d/include/dets_2d_3d_node.hpp
+++ src/perception/tracking/dets_2d_3d/include/dets_2d_3d_node.hpp
@@ -10,11 +10,11 @@
 #include <visualization_msgs/msg/marker_array.hpp>
 
 #include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
 #include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 
-#include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 #include "cluster.hpp"
 #include "projection_utils.hpp"
@@ -23,65 +23,67 @@
 
 struct ClusteringParams;
 
-class TrackingNode : public rclcpp::Node
-{
+class TrackingNode : public rclcpp::Node {
 public:
-    TrackingNode();
+  TrackingNode();
 
 private:
-    std::map<std::string, ClusteringParams> clusteringParams;
+  std::map<std::string, ClusteringParams> clusteringParams;
 
-    // lidar and camera frame names
-    std::string lidarFrame_;
-    std::string cameraFrame_;
+  // lidar and camera frame names
+  std::string lidarFrame_;
+  std::string cameraFrame_;
 
-    // synchronization utils
-    std::mutex lidarCloud_mutex_;
+  // synchronization utils
+  std::mutex lidarCloud_mutex_;
 
-    // camera information cached
-    bool transformInited_;
-    geometry_msgs::msg::TransformStamped transform_;
-    sensor_msgs::msg::CameraInfo::SharedPtr camInfo_;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr lidarCloud_;
+  // camera information cached
+  bool transformInited_;
+  geometry_msgs::msg::TransformStamped transform_;
+  sensor_msgs::msg::CameraInfo::SharedPtr camInfo_;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr lidarCloud_;
 
-    // subscribers to camera intrinsics, lidar pts, camera feed, 2d detection boxes
-    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camInfo_subscriber_;
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_subscriber_;
-    rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr det_subscriber_;
+  // subscribers to camera intrinsics, lidar pts, camera feed, 2d detection
+  // boxes
+  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr
+      camInfo_subscriber_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr
+      lidar_subscriber_;
+  rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr
+      det_subscriber_;
 
-    // publish the 3d detections
-    rclcpp::Publisher<vision_msgs::msg::Detection3DArray>::SharedPtr det3d_publisher_;
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_publisher_;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher_;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher2_;
+  // publish the 3d detections
+  rclcpp::Publisher<vision_msgs::msg::Detection3DArray>::SharedPtr
+      det3d_publisher_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
+      marker_publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher2_;
 
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
-    void readCameraInfo(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
-    void receiveLidar(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-    void receiveDetections(const vision_msgs::msg::Detection2DArray::SharedPtr msg);
+  void readCameraInfo(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
+  void receiveLidar(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+  void
+  receiveDetections(const vision_msgs::msg::Detection2DArray::SharedPtr msg);
 
-    int highestIOUScoredBBox(
-        const std::vector<vision_msgs::msg::BoundingBox3D> bboxes,
-        const vision_msgs::msg::BoundingBox2D& detBBox,
-        const std::vector<std::shared_ptr<Cluster>>& clusters);
-    double overlapBoundingBox(
-        const vision_msgs::msg::BoundingBox2D& bboxA, 
-        const vision_msgs::msg::BoundingBox2D& bboxB);
-    double iouScore(
-        const vision_msgs::msg::BoundingBox2D& bboxA, 
-        const vision_msgs::msg::BoundingBox2D& bboxB);
+  int highestIOUScoredBBox(
+      const std::vector<vision_msgs::msg::BoundingBox3D> bboxes,
+      const vision_msgs::msg::BoundingBox2D &detBBox,
+      const std::vector<std::shared_ptr<Cluster>> &clusters);
+  double overlapBoundingBox(const vision_msgs::msg::BoundingBox2D &bboxA,
+                            const vision_msgs::msg::BoundingBox2D &bboxB);
+  double iouScore(const vision_msgs::msg::BoundingBox2D &bboxA,
+                  const vision_msgs::msg::BoundingBox2D &bboxB);
 
-    std::map<std::string, ClusteringParams> initializeClusteringParams(
-        const std::map<std::string, rclcpp::Parameter>& clustering_params_map);
+  std::map<std::string, ClusteringParams> initializeClusteringParams(
+      const std::map<std::string, rclcpp::Parameter> &clustering_params_map);
 
-    template <typename T>
-    T getDefaultOrValue(std::map<std::string, T> m, std::string key)
-    {
-        if (m.find(key) == m.end())
-            return m[key];
-        return m["default"];
-    }
-
+  template <typename T>
+  T getDefaultOrValue(std::map<std::string, T> m, std::string key) {
+    if (m.find(key) == m.end())
+      return m[key];
+    return m["default"];
+  }
 };
