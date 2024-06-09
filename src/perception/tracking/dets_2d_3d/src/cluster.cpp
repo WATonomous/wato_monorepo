4901dca2f3099dc289c5e798ca0873056da39394
Meshva Desai <meshva03@gmail.com>
2024-06-09 23:01:29 +0000

    linter

--- src/perception/tracking/dets_2d_3d/src/cluster.cpp
+++ src/perception/tracking/dets_2d_3d/src/cluster.cpp
@@ -1,15 +1,15 @@
 #include "cluster.hpp"
 
-#include <pcl/common/transforms.h>
+#include <geometry_msgs/msg/point.hpp>
 #include <pcl/common/centroid.h>
 #include <pcl/common/common.h>
-#include <geometry_msgs/msg/point.hpp>
+#include <pcl/common/transforms.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
 #include <iostream>
@@ -18,103 +18,105 @@ typedef pcl::PointCloud<pcl::PointXYZRGB> ClusterCloud;
 typedef vision_msgs::msg::BoundingBox3D BBox3D;
 
 int Cluster::color_index = 0;
-std::vector<std::vector<int>> Cluster::colors = {{0,0,255}, {0,255,0}, {255,0,0}, {255,255,0}, {0,255,255}, {255,0,255}};
+std::vector<std::vector<int>> Cluster::colors = {{0, 0, 255},   {0, 255, 0},
+                                                 {255, 0, 0},   {255, 255, 0},
+                                                 {0, 255, 255}, {255, 0, 255}};
 
-Cluster::Cluster(
-    const pcl::PointCloud<pcl::PointXYZ>::Ptr& in_cloud_ptr, 
-    const std::vector<int>& in_cluster_indices) : cloud_ {new ClusterCloud()}
-{
-    bool indexesGiven = in_cluster_indices.size() > 0;
+Cluster::Cluster(const pcl::PointCloud<pcl::PointXYZ>::Ptr &in_cloud_ptr,
+                 const std::vector<int> &in_cluster_indices)
+    : cloud_{new ClusterCloud()} {
+  bool indexesGiven = in_cluster_indices.size() > 0;
 
-    size_t max_index = (indexesGiven) ? in_cluster_indices.size() : in_cloud_ptr->points.size();
+  size_t max_index =
+      (indexesGiven) ? in_cluster_indices.size() : in_cloud_ptr->points.size();
 
-    min_point_.x = std::numeric_limits<double>::max();
-    min_point_.y = std::numeric_limits<double>::max();
-    min_point_.z = std::numeric_limits<double>::max();
+  min_point_.x = std::numeric_limits<double>::max();
+  min_point_.y = std::numeric_limits<double>::max();
+  min_point_.z = std::numeric_limits<double>::max();
 
-    max_point_.x = -std::numeric_limits<double>::max();
-    max_point_.y = -std::numeric_limits<double>::max();
-    max_point_.z = -std::numeric_limits<double>::max();
+  max_point_.x = -std::numeric_limits<double>::max();
+  max_point_.y = -std::numeric_limits<double>::max();
+  max_point_.z = -std::numeric_limits<double>::max();
 
-    for (size_t i=0; i<max_index; ++i)
-    {
-        size_t idx = (indexesGiven) ? in_cluster_indices[i] : i;
+  for (size_t i = 0; i < max_index; ++i) {
+    size_t idx = (indexesGiven) ? in_cluster_indices[i] : i;
 
-        pcl::PointXYZRGB pt;
-        pt.x = in_cloud_ptr->points[idx].x;
-        pt.y = in_cloud_ptr->points[idx].y;
-        pt.z = in_cloud_ptr->points[idx].z;
-        pt.r = colors[color_index % colors.size()][0];
-        pt.g = colors[color_index % colors.size()][1];
-        pt.b = colors[color_index % colors.size()][2];
+    pcl::PointXYZRGB pt;
+    pt.x = in_cloud_ptr->points[idx].x;
+    pt.y = in_cloud_ptr->points[idx].y;
+    pt.z = in_cloud_ptr->points[idx].z;
+    pt.r = colors[color_index % colors.size()][0];
+    pt.g = colors[color_index % colors.size()][1];
+    pt.b = colors[color_index % colors.size()][2];
 
-        if (pt.x < min_point_.x) min_point_.x = pt.x;
-        if (pt.y < min_point_.y) min_point_.y = pt.y;
-        if (pt.z < min_point_.z) min_point_.z = pt.z;
+    if (pt.x < min_point_.x)
+      min_point_.x = pt.x;
+    if (pt.y < min_point_.y)
+      min_point_.y = pt.y;
+    if (pt.z < min_point_.z)
+      min_point_.z = pt.z;
 
-        if (pt.x > max_point_.x) max_point_.x = pt.x;
-        if (pt.y > max_point_.y) max_point_.y = pt.y;
-        if (pt.z > max_point_.z) max_point_.z = pt.z;
+    if (pt.x > max_point_.x)
+      max_point_.x = pt.x;
+    if (pt.y > max_point_.y)
+      max_point_.y = pt.y;
+    if (pt.z > max_point_.z)
+      max_point_.z = pt.z;
 
+    cloud_->emplace_back(pt);
+  }
 
-        cloud_->emplace_back(pt);
-    }
-
-    ++color_index;
+  ++color_index;
 }
 
-BBox3D Cluster::getBoundingBox()
-{
-    BBox3D bounding_box_;
-    
-    if (cloud_->size() == 0) return bounding_box_;
+BBox3D Cluster::getBoundingBox() {
+  BBox3D bounding_box_;
 
-    double length_ = max_point_.x - min_point_.x;
-    double width_ = max_point_.y - min_point_.y;
-    double height_ = max_point_.z - min_point_.z;
-
-    bounding_box_.center.position.x = min_point_.x + length_ / 2;
-    bounding_box_.center.position.y = min_point_.y + width_ / 2;
-    bounding_box_.center.position.z = min_point_.z + height_ / 2;
-
-    bounding_box_.size.x = ((length_ < 0) ? -1 * length_ : length_);
-    bounding_box_.size.y = ((width_ < 0) ? -1 * width_ : width_);
-    bounding_box_.size.z = ((height_ < 0) ? -1 * height_ : height_);
-
-    // pose estimation
-    double rz = 0;
-
-    {
-        std::vector<cv::Point2f> points;
-        for (unsigned int i = 0; i < cloud_->points.size(); i++) 
-        {
-            cv::Point2f pt;
-            pt.x = cloud_->points[i].x;
-            pt.y = cloud_->points[i].y;
-            points.push_back(pt);
-        }
-
-        cv::RotatedRect box = cv::minAreaRect(points);
-        rz = box.angle * 3.14 / 180;
-        bounding_box_.center.position.x = box.center.x;
-        bounding_box_.center.position.y = box.center.y;
-        bounding_box_.size.x = box.size.width;
-        bounding_box_.size.y = box.size.height;
-    }
-
-    // set bounding box direction
-    tf2::Quaternion quat;
-    quat.setRPY(0.0, 0.0, rz);
-
-    geometry_msgs::msg::Quaternion msg_quat = tf2::toMsg(quat);
-    bounding_box_.center.orientation = msg_quat;
-
-    std::cout << cv::getBuildInformation() << std::endl;
-
+  if (cloud_->size() == 0)
     return bounding_box_;
+
+  double length_ = max_point_.x - min_point_.x;
+  double width_ = max_point_.y - min_point_.y;
+  double height_ = max_point_.z - min_point_.z;
+
+  bounding_box_.center.position.x = min_point_.x + length_ / 2;
+  bounding_box_.center.position.y = min_point_.y + width_ / 2;
+  bounding_box_.center.position.z = min_point_.z + height_ / 2;
+
+  bounding_box_.size.x = ((length_ < 0) ? -1 * length_ : length_);
+  bounding_box_.size.y = ((width_ < 0) ? -1 * width_ : width_);
+  bounding_box_.size.z = ((height_ < 0) ? -1 * height_ : height_);
+
+  // pose estimation
+  double rz = 0;
+
+  {
+    std::vector<cv::Point2f> points;
+    for (unsigned int i = 0; i < cloud_->points.size(); i++) {
+      cv::Point2f pt;
+      pt.x = cloud_->points[i].x;
+      pt.y = cloud_->points[i].y;
+      points.push_back(pt);
+    }
+
+    cv::RotatedRect box = cv::minAreaRect(points);
+    rz = box.angle * 3.14 / 180;
+    bounding_box_.center.position.x = box.center.x;
+    bounding_box_.center.position.y = box.center.y;
+    bounding_box_.size.x = box.size.width;
+    bounding_box_.size.y = box.size.height;
+  }
+
+  // set bounding box direction
+  tf2::Quaternion quat;
+  quat.setRPY(0.0, 0.0, rz);
+
+  geometry_msgs::msg::Quaternion msg_quat = tf2::toMsg(quat);
+  bounding_box_.center.orientation = msg_quat;
+
+  std::cout << cv::getBuildInformation() << std::endl;
+
+  return bounding_box_;
 }
 
-bool Cluster::isValid()
-{
-    return cloud_->size() > 0;
-}
+bool Cluster::isValid() { return cloud_->size() > 0; }
--- src/perception/tracking/dets_2d_3d/src/dets_2d_3d_node.cpp
+++ src/perception/tracking/dets_2d_3d/src/dets_2d_3d_node.cpp
@@ -1,8 +1,8 @@
 #include "dets_2d_3d_node.hpp"
 
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <pcl/io/pcd_io.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -10,35 +10,43 @@
 #include <iostream>
 #include <memory>
 
-
-TrackingNode::TrackingNode() : Node("dets_2d_3d", rclcpp::NodeOptions().allow_undeclared_parameters(true).automatically_declare_parameters_from_overrides(true)),
-  transformInited_{false} , lidarCloud_{new pcl::PointCloud<pcl::PointXYZ>()}
-{
-  rclcpp::NodeOptions().allow_undeclared_parameters(true).automatically_declare_parameters_from_overrides(true);
+TrackingNode::TrackingNode()
+    : Node("dets_2d_3d",
+           rclcpp::NodeOptions()
+               .allow_undeclared_parameters(true)
+               .automatically_declare_parameters_from_overrides(true)),
+      transformInited_{false}, lidarCloud_{
+                                   new pcl::PointCloud<pcl::PointXYZ>()} {
+  rclcpp::NodeOptions()
+      .allow_undeclared_parameters(true)
+      .automatically_declare_parameters_from_overrides(true);
 
   lidarFrame_ = this->get_parameter("lidar_frame").as_string();
   cameraFrame_ = this->get_parameter("camera_frame").as_string();
 
   // setup pub subs
   camInfo_subscriber_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(
-    this->get_parameter("camera_info_topic").as_string(), 10, 
-    std::bind(&TrackingNode::readCameraInfo, this, std::placeholders::_1));
+      this->get_parameter("camera_info_topic").as_string(), 10,
+      std::bind(&TrackingNode::readCameraInfo, this, std::placeholders::_1));
 
   lidar_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-    this->get_parameter("lidar_topic").as_string(), 10, 
-    std::bind(&TrackingNode::receiveLidar, this, std::placeholders::_1));
+      this->get_parameter("lidar_topic").as_string(), 10,
+      std::bind(&TrackingNode::receiveLidar, this, std::placeholders::_1));
 
-  det_subscriber_ = this->create_subscription<vision_msgs::msg::Detection2DArray>(
-    this->get_parameter("detections_topic").as_string(), 10,
-    std::bind(&TrackingNode::receiveDetections, this, std::placeholders::_1));
+  det_subscriber_ =
+      this->create_subscription<vision_msgs::msg::Detection2DArray>(
+          this->get_parameter("detections_topic").as_string(), 10,
+          std::bind(&TrackingNode::receiveDetections, this,
+                    std::placeholders::_1));
 
   det3d_publisher_ = this->create_publisher<vision_msgs::msg::Detection3DArray>(
-    this->get_parameter("publish_detections_topic").as_string(), 10);
-  marker_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(
-    this->get_parameter("publish_markers_topic").as_string(), 10);
+      this->get_parameter("publish_detections_topic").as_string(), 10);
+  marker_publisher_ =
+      this->create_publisher<visualization_msgs::msg::MarkerArray>(
+          this->get_parameter("publish_markers_topic").as_string(), 10);
   pc_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-    this->get_parameter("publish_clusters_topic").as_string(), 10);
-  
+      this->get_parameter("publish_clusters_topic").as_string(), 10);
+
   tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
   tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
@@ -47,12 +55,12 @@ TrackingNode::TrackingNode() : Node("dets_2d_3d", rclcpp::NodeOptions().allow_un
   clusteringParams = initializeClusteringParams(clustering_params_map);
 }
 
-std::map<std::string, ClusteringParams> TrackingNode::initializeClusteringParams(
-  const std::map<std::string, rclcpp::Parameter>& clustering_params_map)
-{
+std::map<std::string, ClusteringParams>
+TrackingNode::initializeClusteringParams(
+    const std::map<std::string, rclcpp::Parameter> &clustering_params_map) {
   std::map<std::string, ClusteringParams> clusteringParams;
-  for(auto iter = clustering_params_map.begin(); iter != clustering_params_map.end(); ++iter)
-  {
+  for (auto iter = clustering_params_map.begin();
+       iter != clustering_params_map.end(); ++iter) {
     std::string key = iter->first;
     size_t split_loc = key.find_first_of('.');
     std::string det_type = key.substr(0, split_loc);
@@ -75,8 +83,8 @@ std::map<std::string, ClusteringParams> TrackingNode::initializeClusteringParams
   return clusteringParams;
 }
 
-void TrackingNode::receiveLidar(const sensor_msgs::msg::PointCloud2::SharedPtr pointCloud_msg)
-{
+void TrackingNode::receiveLidar(
+    const sensor_msgs::msg::PointCloud2::SharedPtr pointCloud_msg) {
   // save latest lidar info
   std::lock_guard<std::mutex> guard_lidar(lidarCloud_mutex_);
 
@@ -84,89 +92,103 @@ void TrackingNode::receiveLidar(const sensor_msgs::msg::PointCloud2::SharedPtr p
   pcl::fromROSMsg(pointCloud, *lidarCloud_);
 }
 
-void TrackingNode::readCameraInfo(const sensor_msgs::msg::CameraInfo::SharedPtr msg)
-{
+void TrackingNode::readCameraInfo(
+    const sensor_msgs::msg::CameraInfo::SharedPtr msg) {
   std::cout << msg->distortion_model << std::endl;
   camInfo_ = msg;
   camInfo_subscriber_.reset();
 }
 
-void TrackingNode::receiveDetections(const vision_msgs::msg::Detection2DArray::SharedPtr msg)
-{
-  if (!transformInited_)
-  {
+void TrackingNode::receiveDetections(
+    const vision_msgs::msg::Detection2DArray::SharedPtr msg) {
+  if (!transformInited_) {
     try {
-      transform_ = tf_buffer_ ->lookupTransform(cameraFrame_, lidarFrame_, msg->header.stamp);
+      transform_ = tf_buffer_->lookupTransform(cameraFrame_, lidarFrame_,
+                                               msg->header.stamp);
       transformInited_ = true;
-    } catch (const tf2::TransformException & ex) {
+    } catch (const tf2::TransformException &ex) {
       RCLCPP_INFO(this->get_logger(), "Could not transform %s", ex.what());
       return;
     }
   }
 
   // remove floor from lidar cloud
-  pcl::PointCloud<pcl::PointXYZ>::Ptr lidarCloudNoFloor(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr lidarCloudNoFloor(
+      new pcl::PointCloud<pcl::PointXYZ>());
   {
     std::lock_guard<std::mutex> guard_lidar(lidarCloud_mutex_);
     ProjectionUtils::removeFloor(lidarCloud_, lidarCloudNoFloor);
 
-    if (lidarCloudNoFloor->size() == 0) return; // do not process more, if all the points are floor points
-    RCLCPP_INFO(this->get_logger(), "receive detection %ld vs %ld", lidarCloud_->size(), lidarCloudNoFloor->size());
+    if (lidarCloudNoFloor->size() == 0)
+      return; // do not process more, if all the points are floor points
+    RCLCPP_INFO(this->get_logger(), "receive detection %ld vs %ld",
+                lidarCloud_->size(), lidarCloudNoFloor->size());
   }
 
   // transform all lidar pts to 2d points in the camera
   std::vector<geometry_msgs::msg::Point> lidar2dProjs;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr inCameraPoints(new pcl::PointCloud<pcl::PointXYZ>());
-  for (const pcl::PointXYZ& pt : lidarCloudNoFloor->points)
-  {    
-    std::optional<geometry_msgs::msg::Point> proj = ProjectionUtils::projectLidarToCamera(transform_, camInfo_->p, pt);
-    if (proj) 
-    {
+  pcl::PointCloud<pcl::PointXYZ>::Ptr inCameraPoints(
+      new pcl::PointCloud<pcl::PointXYZ>());
+  for (const pcl::PointXYZ &pt : lidarCloudNoFloor->points) {
+    std::optional<geometry_msgs::msg::Point> proj =
+        ProjectionUtils::projectLidarToCamera(transform_, camInfo_->p, pt);
+    if (proj) {
       inCameraPoints->emplace_back(pt);
       lidar2dProjs.emplace_back(*proj);
     }
   }
-  
+
   // publish both rviz markers (for visualization) and a detection array
   visualization_msgs::msg::MarkerArray markerArray3d;
   vision_msgs::msg::Detection3DArray detArray3d;
-  pcl::PointCloud<pcl::PointXYZRGB> mergedClusters; // coloured pc to visualize points in each cluster
+  pcl::PointCloud<pcl::PointXYZRGB>
+      mergedClusters; // coloured pc to visualize points in each cluster
 
   // process each detection in det array
   int bboxId = 0;
-  for (const vision_msgs::msg::Detection2D& det : msg->detections)
-  {
+  for (const vision_msgs::msg::Detection2D &det : msg->detections) {
     vision_msgs::msg::BoundingBox2D bbox = det.bbox;
 
     // process lidarcloud with extrinsic trans from lidar to cam frame
-    pcl::PointCloud<pcl::PointXYZ>::Ptr inlierPoints(new pcl::PointCloud<pcl::PointXYZ>());
-    ProjectionUtils::pointsInBbox(inlierPoints, inCameraPoints, lidar2dProjs, bbox);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inlierPoints(
+        new pcl::PointCloud<pcl::PointXYZ>());
+    ProjectionUtils::pointsInBbox(inlierPoints, inCameraPoints, lidar2dProjs,
+                                  bbox);
     if (inlierPoints->size() == 0) {
-      RCLCPP_INFO(this->get_logger(), "no inliers found for detection %s", det.results[0].hypothesis.class_id.c_str());
+      RCLCPP_INFO(this->get_logger(), "no inliers found for detection %s",
+                  det.results[0].hypothesis.class_id.c_str());
       continue;
     }
 
     // clustering
-    auto clusterAndBBoxes = ProjectionUtils::getClusteredBBoxes(inlierPoints, getDefaultOrValue<ClusteringParams>(clusteringParams, det.results[0].hypothesis.class_id.c_str()));
-    std::vector<std::shared_ptr<Cluster>> clusters = clusterAndBBoxes.first; // needed? for viz purposes only
-    std::vector<vision_msgs::msg::BoundingBox3D> allBBoxes = clusterAndBBoxes.second;
+    auto clusterAndBBoxes = ProjectionUtils::getClusteredBBoxes(
+        inlierPoints,
+        getDefaultOrValue<ClusteringParams>(
+            clusteringParams, det.results[0].hypothesis.class_id.c_str()));
+    std::vector<std::shared_ptr<Cluster>> clusters =
+        clusterAndBBoxes.first; // needed? for viz purposes only
+    std::vector<vision_msgs::msg::BoundingBox3D> allBBoxes =
+        clusterAndBBoxes.second;
 
-    if (clusters.size() == 0 || allBBoxes.size() == 0) continue;
+    if (clusters.size() == 0 || allBBoxes.size() == 0)
+      continue;
 
-    // score each 3d bbox based on iou with teh original 2d bbox & the density of points in the cluster
+    // score each 3d bbox based on iou with teh original 2d bbox & the density
+    // of points in the cluster
     int bestIndex = highestIOUScoredBBox(allBBoxes, bbox, clusters);
     vision_msgs::msg::BoundingBox3D bestBBox = allBBoxes[bestIndex];
 
-    // for visauzliation : adds detection to cluster pc visualization & the marker array & the 3d detection array
+    // for visauzliation : adds detection to cluster pc visualization & the
+    // marker array & the 3d detection array
     mergedClusters += *(clusters[bestIndex]->getCloud());
 
     vision_msgs::msg::Detection3D det3d;
-    det3d.bbox  = bestBBox;
+    det3d.bbox = bestBBox;
     det3d.results = det.results;
     detArray3d.detections.emplace_back(det3d);
 
     visualization_msgs::msg::Marker marker;
-    marker.type =  visualization_msgs::msg::Marker::CUBE;
+    marker.type = visualization_msgs::msg::Marker::CUBE;
     marker.scale = bestBBox.size;
     marker.pose = bestBBox.center;
     marker.header.frame_id = lidarFrame_;
@@ -186,40 +208,43 @@ void TrackingNode::receiveDetections(const vision_msgs::msg::Detection2DArray::S
 
   pc_publisher_->publish(pubCloud);
 
-  RCLCPP_INFO(this->get_logger(), "published %ld 3d detections\n\n", markerArray3d.markers.size());
-  
+  RCLCPP_INFO(this->get_logger(), "published %ld 3d detections\n\n",
+              markerArray3d.markers.size());
 }
 
 int TrackingNode::highestIOUScoredBBox(
-  const std::vector<vision_msgs::msg::BoundingBox3D> bboxes,
-  const vision_msgs::msg::BoundingBox2D& detBBox,
-  const std::vector<std::shared_ptr<Cluster>>& clusters)
-{
+    const std::vector<vision_msgs::msg::BoundingBox3D> bboxes,
+    const vision_msgs::msg::BoundingBox2D &detBBox,
+    const std::vector<std::shared_ptr<Cluster>> &clusters) {
   int bestScore = 0;
   int bestBBox = 0;
 
-  for (size_t i=0; i<bboxes.size(); ++i)
-  {
+  for (size_t i = 0; i < bboxes.size(); ++i) {
     vision_msgs::msg::BoundingBox3D b = bboxes[i];
     // project the 3d corners of the bbox to 2d camera frame
     pcl::PointXYZ top_left;
-    top_left.x = b.center.position.x - b.size.x/2;
-    top_left.y = b.center.position.y - b.size.y/2;
-    top_left.z = b.center.position.z - b.size.z/2;
+    top_left.x = b.center.position.x - b.size.x / 2;
+    top_left.y = b.center.position.y - b.size.y / 2;
+    top_left.z = b.center.position.z - b.size.z / 2;
 
     pcl::PointXYZ bottom_right;
-    bottom_right.x = b.center.position.x + b.size.x/2;
-    bottom_right.y = b.center.position.y + b.size.y/2;
-    bottom_right.z = b.center.position.z + b.size.z/2;
+    bottom_right.x = b.center.position.x + b.size.x / 2;
+    bottom_right.y = b.center.position.y + b.size.y / 2;
+    bottom_right.z = b.center.position.z + b.size.z / 2;
 
-    std::optional<geometry_msgs::msg::Point> top_left2d = ProjectionUtils::projectLidarToCamera(transform_, camInfo_->p, top_left);
-    std::optional<geometry_msgs::msg::Point> bottom_right2d = ProjectionUtils::projectLidarToCamera(transform_, camInfo_->p, bottom_right);
+    std::optional<geometry_msgs::msg::Point> top_left2d =
+        ProjectionUtils::projectLidarToCamera(transform_, camInfo_->p,
+                                              top_left);
+    std::optional<geometry_msgs::msg::Point> bottom_right2d =
+        ProjectionUtils::projectLidarToCamera(transform_, camInfo_->p,
+                                              bottom_right);
 
-    if (!top_left2d || !bottom_right2d) return 0;
+    if (!top_left2d || !bottom_right2d)
+      return 0;
 
     vision_msgs::msg::BoundingBox2D bbox2d;
-    bbox2d.center.position.x = ((top_left2d->x) + (bottom_right2d->x))/2;
-    bbox2d.center.position.y = ((top_left2d->y) + (bottom_right2d->y))/2;
+    bbox2d.center.position.x = ((top_left2d->x) + (bottom_right2d->x)) / 2;
+    bbox2d.center.position.y = ((top_left2d->y) + (bottom_right2d->y)) / 2;
     bbox2d.size_x = abs(top_left2d->x - bottom_right2d->x);
     bbox2d.size_y = abs(top_left2d->y - bottom_right2d->y);
 
@@ -229,8 +254,7 @@ int TrackingNode::highestIOUScoredBBox(
     double density = clusters[i]->size() / (b.size.x * b.size.y * b.size.z);
     double score = iou + density;
 
-    if (score > bestScore)
-    {
+    if (score > bestScore) {
       bestScore = score;
       bestBBox = i;
     }
@@ -238,30 +262,34 @@ int TrackingNode::highestIOUScoredBBox(
   return bestBBox;
 }
 
-double TrackingNode::overlapBoundingBox(const vision_msgs::msg::BoundingBox2D& boxA, const vision_msgs::msg::BoundingBox2D& boxB)
-{
-  double overlapHeight =
-      std::min(boxA.center.position.y + boxA.size_y/2, boxB.center.position.y + boxB.size_y/2) -
-      std::max(boxA.center.position.y - boxA.size_y/2, boxB.center.position.y - boxB.size_y/2);
-  if (overlapHeight <= 0) return 0;
-  
-  double overlapWidth = std::min(boxA.center.position.x + boxA.size_x/2, boxB.center.position.x + boxB.size_x/2) -
-                        std::max(boxA.center.position.x - boxA.size_x/2, boxB.center.position.x - boxB.size_x/2);
-  if (overlapWidth <= 0) return 0;
-  
+double
+TrackingNode::overlapBoundingBox(const vision_msgs::msg::BoundingBox2D &boxA,
+                                 const vision_msgs::msg::BoundingBox2D &boxB) {
+  double overlapHeight = std::min(boxA.center.position.y + boxA.size_y / 2,
+                                  boxB.center.position.y + boxB.size_y / 2) -
+                         std::max(boxA.center.position.y - boxA.size_y / 2,
+                                  boxB.center.position.y - boxB.size_y / 2);
+  if (overlapHeight <= 0)
+    return 0;
+
+  double overlapWidth = std::min(boxA.center.position.x + boxA.size_x / 2,
+                                 boxB.center.position.x + boxB.size_x / 2) -
+                        std::max(boxA.center.position.x - boxA.size_x / 2,
+                                 boxB.center.position.x - boxB.size_x / 2);
+  if (overlapWidth <= 0)
+    return 0;
+
   return overlapHeight * overlapWidth;
 }
 
-double TrackingNode::iouScore(const vision_msgs::msg::BoundingBox2D& bboxA, const vision_msgs::msg::BoundingBox2D& bboxB)
-{
+double TrackingNode::iouScore(const vision_msgs::msg::BoundingBox2D &bboxA,
+                              const vision_msgs::msg::BoundingBox2D &bboxB) {
   double overlap = overlapBoundingBox(bboxA, bboxB);
-  return (overlap /
-          (bboxA.size_x * bboxA.size_y + bboxB.size_x * bboxB.size_y - overlap));
-
+  return (overlap / (bboxA.size_x * bboxA.size_y + bboxB.size_x * bboxB.size_y -
+                     overlap));
 }
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<TrackingNode>());
   rclcpp::shutdown();
