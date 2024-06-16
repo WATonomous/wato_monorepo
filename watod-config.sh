dfad0f126f132abed71c9437e6606f6bcfb2de9c
Lucas Reljic-Dumont <56940300+lucasreljic@users.noreply.github.com>
2024-06-16 18:54:47 -0400

    Update watod-config.sh

--- watod-config.sh
+++ watod-config.sh
@@ -15,7 +15,7 @@
 ##	 - simulation			:	starts simulation
 ##   - samples             	:   starts sample ROS2 pubsub nodes
 
-ACTIVE_MODULES=""
+# ACTIVE_MODULES=""
 
 ################################# MODE OF OPERATION #################################
 ## Possible modes of operation when running watod.
@@ -27,11 +27,11 @@ ACTIVE_MODULES=""
 
 ############################## ADVANCED CONFIGURATIONS ##############################
 ## Name to append to docker containers. DEFAULT = "<your_watcloud_username>"
-COMPOSE_PROJECT_NAME=""
+# COMPOSE_PROJECT_NAME=""
 
 ## Tag to use. Images are formatted as <IMAGE_NAME>:<TAG> with forward slashes replaced with dashes.
 ## DEFAULT = "<your_current_github_branch>"
-TAG=""
+# TAG=""
 
 # Docker Registry to pull/push images. DEFAULT = "ghcr.io/watonomous/wato_monorepo"
 # REGISTRY_URL=""
