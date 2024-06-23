0a4d547a368e97b9c1ba725d33ff518e5e74943e
akilpath <akilpath@gmail.com>
2024-06-23 17:17:14 +0000

    Finished czm initialization

--- src/world_modeling/occupancy_segmentation/src/occupancy_segmentation_core.cpp
+++ src/world_modeling/occupancy_segmentation/src/occupancy_segmentation_core.cpp
@@ -24,22 +24,34 @@ void OccupancySegmentationCore::init_czm(){
 }
 
 void OccupancySegmentationCore::fill_czm(pcl::PointCloud<pcl::PointXYZ> &cloud_in){
+    double lmins[4] = {L_MIN, (7*L_MIN + L_MAX) / 8, (3*L_MIN + L_MAX) / 4, (L_MIN + L_MAX) / 2};
+    double lmaxs[4] = {lmins[1], lmins[2], lmins[3], L_MAX};
     for(pcl::PointXYZ &p : cloud_in.points){
         double r = sqrt(pow(p.x, 2) + pow(p.y, 2));
-        double theta = atan2(p.y,p.x); // EDITED!
+
+        // Out of range for czm
+        if (r < L_MIN || r > L_MAX){
+            continue;
+        }
+
+        double theta = atan2(p.y,p.x); 
         if (theta < 0){
             theta += 2*M_PI;
         }
-        double lmins[4] = {L_MIN, (7*L_MIN + L_MAX) / 8, (3*L_MIN + L_MAX) / 4, (L_MIN + L_MAX) / 2};
-        double lmaxs[4] = {lmins[1], lmins[2], lmins[3], L_MAX};
+
         double deltal = L_MAX - L_MIN;
         
         for (int zone_idx = 0; zone_idx < NUM_ZONES; zone_idx++){
-
+            int ring_idx, sector_idx;
+            if (r < lmaxs[zone_idx]){
+                double ring_size = deltal / ZONE_RINGS[zone_idx];
+                double sector_size = 2*M_PI / ZONE_SECTORS[zone_idx];
+                ring_idx = (int) ((r - lmins[zone_idx]) / ring_size);
+                //TODO: find out why the use min() in the paper, meantime use the top
+                //ring_idx = std::min((int) ((r - lmins[zone_idx]) / ring_size),);
+                sector_idx = (int) (theta / sector_size);
+                czm[zone_idx][ring_idx][sector_idx].points.emplace_back(p);
+            }
         }
-
-
-
     }
-
 }
--- watod-config.sh
+++ watod-config.sh
@@ -23,7 +23,7 @@ ACTIVE_MODULES="world_modeling"
 ##	 - deploy (default)		:	runs production-grade containers (non-editable)
 ##	 - develop   		    :	runs developer containers (editable)
 
-# MODE_OF_OPERATION=""
+MODE_OF_OPERATION="develop"
 
 ############################## ADVANCED CONFIGURATIONS ##############################
 ## Name to append to docker containers. DEFAULT = "<your_watcloud_username>"
