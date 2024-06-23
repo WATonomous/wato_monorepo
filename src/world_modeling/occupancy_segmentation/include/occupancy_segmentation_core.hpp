f9e0f33176b2f9395b9c963d1162b613f64184b0
akilpath <akilpath@gmail.com>
2024-06-23 04:32:03 +0000

    Started working on it

--- src/world_modeling/occupancy_segmentation/include/occupancy_segmentation_core.hpp
+++ src/world_modeling/occupancy_segmentation/include/occupancy_segmentation_core.hpp
@@ -2,6 +2,17 @@
 #define OCCUPANCY_SEGMENTATION_CORE_HPP_
 
 #include <vector>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/centroid.h>
+#include <pcl/io/pcd_io.h>
+
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
+
+#define NUM_ZONES 4
+#define L_MIN 2.7
+#define L_MAX 80.0
 
 
 /**
@@ -9,15 +20,29 @@
  * node performing data processing and validation.
  */
 class OccupancySegmentationCore {
- public:
-  // Size of buffer before processed messages are published.
-  static constexpr int BUFFER_CAPACITY = 10;
+  public:
+    typedef std::vector<pcl::PointCloud<pcl::PointXYZ>> Ring;
+    typedef std::vector<Ring> Zone;
+    // Size of buffer before processed messages are published.
+    static constexpr int BUFFER_CAPACITY = 10;
 
- public:
-  /**
-   * Transformer constructor.
-   */
-  OccupancySegmentationCore();
+    const int ZONE_RINGS [NUM_ZONES] = {2,4,4,4};
+    const int ZONE_SECTORS [NUM_ZONES] = {16,32,54,32};
+
+
+    std::vector<Zone> czm;
+
+
+    OccupancySegmentationCore();
+  
+  private:
+
+    void init_czm();
+
+    void fill_czm(pcl::PointCloud<pcl::PointXYZ> &cloud_in);
+
+
+
 };
 
 
