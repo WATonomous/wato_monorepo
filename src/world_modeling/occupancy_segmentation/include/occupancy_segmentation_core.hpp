23aafe4f036c59d36cf22e897fabc7ebb6f02d1c
akilpath <akilpath@gmail.com>
2024-07-07 22:57:54 +0000

    Mostly done

--- src/world_modeling/occupancy_segmentation/include/occupancy_segmentation_core.hpp
+++ src/world_modeling/occupancy_segmentation/include/occupancy_segmentation_core.hpp
@@ -20,6 +20,11 @@
 #define MD 0.3
 #define MH -1.1
 
+#define MIN_NUM_POINTS 10
+#define NUM_SEED_POINTS 20
+#define TH_SEEDS 0.5
+#define UPRIGHTNESS_THRESH 45
+
 
 struct PCAFeature {
     Eigen::Vector3f principal_;
@@ -32,6 +37,14 @@ struct PCAFeature {
     float    planarity_;
 };
 
+struct Patch_Index {
+  int idx;
+  int zone_idx;
+  int ring_idx;
+  int sector_idx;
+  int concentric_idx;
+}
+
 class OccupancySegmentationCore {
   public:
     typedef std::vector<pcl::PointCloud<pcl::PointXYZ>> Ring;
@@ -42,8 +55,16 @@ class OccupancySegmentationCore {
     const int ZONE_RINGS [NUM_ZONES] = {2,4,4,4};
     const int ZONE_SECTORS [NUM_ZONES] = {16,32,54,32};
 
+    int num_patches = -1;
 
-    std::vector<Zone> czm;
+    std::vector<Zone> _czm;
+    std::vector<pcl::PointCloud<pcl::PointXYZ>> _regionwise_ground;
+    std::vector<pcl::PointCloud<pcl::PointXYZ>> _regionwise_nonground;
+
+    std::vector<Patch_Idx> _patch_indices;
+
+    pcl::PointCloud<pcl::PointXYZ> _ground;
+    pcl::Pointcloud<pcl::PointXYZ> _non_ground;
 
 
     OccupancySegmentationCore();
@@ -55,6 +76,16 @@ class OccupancySegmentationCore {
     void fill_czm(pcl::PointCloud<pcl::PointXYZ> &cloud_in);
 
     void estimate_plane(pcl::PointCloud<pcl::PointXYZ> &cloud, PCAFeature &feat);
+     
+    void rgpf(pcl::PointCloud<pcl::PointXYZ> &patch, Patch_Index &p_idx, PCAFeature &feat);
+    
+    void extract_initial_seeds(pcl::PointCloud<pcl::PointXYZ> &cloud, pcl::PointCloud<pcl::PointXYZ> &seed_cloud, int zone_idx);
+    
+    bool ground_likelihood_est(PCAFeature &feat, int concentric_idx);
+
+    void segment_ground();
+
+    bool point_z_cmp(pcl::PointXYZ a, pcl::PointXYZ b);
 
 
 
