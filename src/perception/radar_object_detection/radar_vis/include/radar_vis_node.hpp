d8c302ca10b3bdf342544e84d39e6c86c625b435
Meshva Desai <meshva03@gmail.com>
2024-07-14 16:29:39 +0000

    add arrows for radar viz

--- src/perception/radar_object_detection/radar_vis/include/radar_vis_node.hpp
+++ src/perception/radar_object_detection/radar_vis/include/radar_vis_node.hpp
@@ -4,6 +4,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "sensor_msgs/msg/point_field.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 #include "radar_msgs/msg/radar_detection.hpp"
 #include "radar_msgs/msg/radar_packet.hpp"
@@ -32,6 +34,9 @@ class RadarVisNode : public rclcpp::Node {
 
   // ROS2 Publisher that sends point cloud data to "visualization" topic for foxglove/Rviz.
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr raw_pub_;
+
+  // ROS2 Publisher that sends marker arrow data for foxglove/Rviz.
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
 
   // An object containing methods for converting radar packets into point clouds.
   visualization::RadarVis packet_converter_;
