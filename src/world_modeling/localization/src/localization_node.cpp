4ed19841335fa15ac9d6eb21b564a7c4b0fadba0
Harsh Patel <its.hp3@gmail.com>
2024-07-16 02:43:59 +0000

    fixed gtsam linking issue

--- src/world_modeling/localization/src/localization_node.cpp
+++ src/world_modeling/localization/src/localization_node.cpp
@@ -12,9 +12,6 @@ LocalizationNode::LocalizationNode() : Node("sample"), sample_(world_modeling::h
       "/pose", ADVERTISING_FREQ,
       std::bind(&LocalizationNode::sample_pose_callback, this, std::placeholders::_1));
 
-  sample_pub_ =
-      this->create_publisher<sample_msgs::msg::Unfiltered>("hd_map_sample_topic", ADVERTISING_FREQ);
-
   // Define the default values for parameters if not defined in params.yaml
   this->declare_parameter("version", rclcpp::ParameterValue(0));
   this->declare_parameter("compression_method", rclcpp::ParameterValue(0));
@@ -29,13 +26,6 @@ void LocalizationNode::sample_pose_callback(const geometry_msgs::msg::PoseStampe
 {
   RCLCPP_INFO(this->get_logger(), "Recieved POSE data.. \n");
   RCLCPP_INFO(this->get_logger(), "x: %f, y: %f, z: %f\n", msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
-}
-
-void LocalizationNode::sample_publish(const sample_msgs::msg::Unfiltered::SharedPtr msg)
-{
-  auto pub_msg = sample_msgs::msg::Unfiltered();
-  RCLCPP_INFO(this->get_logger(), "Publishing Sample Message from Localization...\n");
-  sample_pub_->publish(pub_msg);
 }
 
 int main(int argc, char **argv)
