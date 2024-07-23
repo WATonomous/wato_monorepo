c3e1a0b64d80653566bee656c934db645578bc11
Meshva Desai <meshva03@gmail.com>
2024-07-23 04:39:23 +0000

    add params for hypothetical radar

--- src/interfacing/sensor_interfacing/setup.sh
+++ src/interfacing/sensor_interfacing/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 # Display configuration
 echo Using following configuration
-echo VCAN_INTERFACE: "${VCAN_INTERFACE:=can0}"
+echo VCAN_INTERFACE: "${VCAN_INTERFACE:=vcan0}"
 echo VCAN_SOURCE_PORT: "${VCAN_SOURCE_PORT:=6000}"
 echo VCAN_TARGET_IP: "${VCAN_TARGET_IP:=172.17.0.1}"
 echo VCAN_TARGET_PORT: "$VCAN_TARGET_PORT"
