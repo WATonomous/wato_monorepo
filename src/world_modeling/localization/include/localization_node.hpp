f7c694339493cdee66a00abe3d189c9d030c5ad0
Harsh Patel <its.hp3@gmail.com>
2024-10-10 16:03:46 +0000

    added gps factors and initial pose estimates with odometry

--- src/world_modeling/localization/include/localization_node.hpp
+++ src/world_modeling/localization/include/localization_node.hpp
@@ -7,6 +7,17 @@
 
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+#include <GeographicLib/LocalCartesian.hpp>
+
+// GTSAM
+#include <gtsam/geometry/Point3.h>
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/navigation/GPSFactor.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/Values.h>
 
 class LocalizationNode : public rclcpp::Node
 {
@@ -15,23 +26,29 @@ public:
   // https://docs.ros.org/en/foxy/Concepts/About-Quality-of-Service-Settings.html
   static constexpr int ADVERTISING_FREQ = 20;
 
-  /**
-   * Sample node constructor.
-   */
   LocalizationNode();
 
 private:
-  void sample_gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
-  void sample_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+  void setupReference(const sensor_msgs::msg::NavSatFix::SharedPtr ref_point);
+  gtsam::Point3 convertGPSToENU(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
+  void optimize();
 
-  // ROS2 subscribers
+  void gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
+  void odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
   rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_sub_;
-  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
 
-  // ROS2 publishers
-
-  // Object that handles data processing and validation.
+  // Sample object that handles data processing and validation
   world_modeling::hd_map::Sample sample_;
+
+  gtsam::NonlinearFactorGraph graph_;
+  gtsam::Values initial_estimate_;
+  gtsam::noiseModel::Diagonal::shared_ptr gps_noise_model_;
+  GeographicLib::LocalCartesian enu_;
+
+  int odom_counter_;
+  int gps_counter_;
 };
 
 #endif // LOCALIZATION_NODE_HPP_
--- src/world_modeling/localization/src/localization_node.cpp
+++ src/world_modeling/localization/src/localization_node.cpp
@@ -2,30 +2,100 @@
 
 #include "localization_node.hpp"
 
-LocalizationNode::LocalizationNode() : Node("sample"), sample_(world_modeling::hd_map::Sample())
+LocalizationNode::LocalizationNode() : Node("sample"), sample_(world_modeling::hd_map::Sample()), odom_counter_(0), gps_counter_(0)
 {
   gps_sub_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(
       "/gps", ADVERTISING_FREQ,
-      std::bind(&LocalizationNode::sample_gps_callback, this, std::placeholders::_1));
+      std::bind(&LocalizationNode::gps_callback, this, std::placeholders::_1));
 
-  pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-      "/pose", ADVERTISING_FREQ,
-      std::bind(&LocalizationNode::sample_pose_callback, this, std::placeholders::_1));
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/odom", ADVERTISING_FREQ,
+      std::bind(&LocalizationNode::odometry_callback, this, std::placeholders::_1));
 
   // Define the default values for parameters if not defined in params.yaml
   this->declare_parameter("version", rclcpp::ParameterValue(0));
   this->declare_parameter("compression_method", rclcpp::ParameterValue(0));
+
+  // Set the default noise model for the GPS measurements
+  gps_noise_model_ = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector3(0.3, 0.3, 0.1));
+
+  initial_estimate_ = gtsam::Values();
+
+  enu_ = GeographicLib::LocalCartesian(0, 0, 0); // Default reference point - call setupReference() to change
 }
 
-void LocalizationNode::sample_gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
+void LocalizationNode::setupReference(const sensor_msgs::msg::NavSatFix::SharedPtr ref_point)
 {
-  RCLCPP_INFO(this->get_logger(), "Recieved GPS data.. \n");
+  double ref_lat = ref_point->latitude;
+  double ref_lon = ref_point->longitude;
+  double ref_alt = ref_point->altitude;
+
+  // Set the reference point for the ENU frame
+  enu_ = GeographicLib::LocalCartesian(ref_lat, ref_lon, ref_alt);
 }
 
-void LocalizationNode::sample_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+gtsam::Point3 LocalizationNode::convertGPSToENU(const sensor_msgs::msg::NavSatFix::SharedPtr gpsPoint)
 {
-  RCLCPP_INFO(this->get_logger(), "Recieved POSE data.. \n");
-  RCLCPP_INFO(this->get_logger(), "x: %f, y: %f, z: %f\n", msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
+  double lat = gpsPoint->latitude;
+  double lon = gpsPoint->longitude;
+  double alt = gpsPoint->altitude;
+
+  // Convert GPS to ENU
+  double x, y, z;
+  enu_.Forward(lat, lon, alt, x, y, z);
+
+  return gtsam::Point3(x, y, z);
+}
+
+void LocalizationNode::optimize()
+{
+  // Create a Levenberg-Marquardt optimizer
+  gtsam::LevenbergMarquardtOptimizer optimizer(graph_, initial_estimate_);
+
+  gtsam::Values result = optimizer.optimize();
+
+  result.print("Final result:\n");
+}
+
+void LocalizationNode::gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
+{
+  gtsam::Point3 enu_point = convertGPSToENU(msg);
+
+  RCLCPP_INFO(this->get_logger(), "Recieved GPS data %d.. \n", gps_counter_);
+  RCLCPP_INFO(this->get_logger(), "Lat: %f, Long: %f, Alt: %f\n", msg->latitude, msg->longitude,
+              msg->altitude);
+  RCLCPP_INFO(this->get_logger(), "ENU: x: %f, y: %f, z: %f\n", enu_point.x(), enu_point.y(),
+              enu_point.z());
+
+  // Add the GPS measurement to the graph as a factor
+  graph_.add(gtsam::GPSFactor(gps_counter_, enu_point, gps_noise_model_));
+
+  gps_counter_++;
+}
+
+void LocalizationNode::odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+  // Extract position (x, y, z) and orientation (roll, pitch, yaw) from the odometry message
+  double x = msg->pose.pose.position.x;
+  double y = msg->pose.pose.position.y;
+  double z = msg->pose.pose.position.z;
+
+  gtsam::Rot3 rotation = gtsam::Rot3::Quaternion(
+      msg->pose.pose.orientation.w,
+      msg->pose.pose.orientation.x,
+      msg->pose.pose.orientation.y,
+      msg->pose.pose.orientation.z);
+
+  RCLCPP_INFO(this->get_logger(), "Received 3D odometry data %d.. \n", odom_counter_);
+  RCLCPP_INFO(this->get_logger(), "Odometry pose: x: %f, y: %f, z: %f\n", x, y, z);
+
+  // Create Pose3 from odometry data (translation and rotation)
+  gtsam::Pose3 odom_pose(rotation, gtsam::Point3(x, y, z));
+
+  // Add initial pose guess to the graph
+  initial_estimate_.insert(odom_counter_, odom_pose);
+
+  odom_counter_++;
 }
 
 int main(int argc, char **argv)
