84911d7a119d396cbb4b6fb60b5fc9e7e84f40ae
VishGit1234 <vishalschool1234@gmail.com>
2024-10-16 22:24:00 +0000

    Template for robot_localization

---
+++ src/world_modeling/localization/launch/localization.launch.py
@@ -0,0 +1,15 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+import os
+
+
+def generate_launch_description():
+    """Launch localization node."""
+
+    return LaunchDescription([
+      
+    ])
