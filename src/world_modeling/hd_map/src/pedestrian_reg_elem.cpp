0c0b4b544213c3d34c83f18cd284b4abc9012fa9
VishGit1234 <vishalschool1234@gmail.com>
2024-10-17 00:30:24 +0000

    Get hd_map to build

--- src/world_modeling/hd_map/src/pedestrian_reg_elem.cpp
+++ src/world_modeling/hd_map/src/pedestrian_reg_elem.cpp
@@ -1,30 +1,10 @@
 #include "pedestrian_reg_elem.hpp"
-
-lanelet::Polygon3d boundingBox3dToPolygon3d(const lanelet::BoundingBox3d& bbox){
-    auto min = bbox.min();
-    auto max = bbox.max();
-
-    lanelet::Polygon3d polygon{
-        lanelet::utils::getId(),
-        {
-          lanelet::Point3d(lanelet::utils::getId(), min.x(), min.y(), min.z()),
-          lanelet::Point3d(lanelet::utils::getId(), max.x(), min.y(), min.z()),
-          lanelet::Point3d(lanelet::utils::getId(), max.x(), max.y(), min.z()),
-          lanelet::Point3d(lanelet::utils::getId(), min.x(), max.y(), min.z()),
-          lanelet::Point3d(lanelet::utils::getId(), min.x(), min.y(), max.z()),
-          lanelet::Point3d(lanelet::utils::getId(), max.x(), min.y(), max.z()),
-          lanelet::Point3d(lanelet::utils::getId(), max.x(), max.y(), max.z()),
-          lanelet::Point3d(lanelet::utils::getId(), min.x(), max.y(), max.z())
-        }
-    };
-
-    return polygon;
-}
+#include "utils.hpp"
 
 // Static factory method to create a new instance
 std::shared_ptr<PedestrianRegElem> PedestrianRegElem::make(const lanelet::BoundingBox3d& pedestrianBBox, uint64_t id) {
     // Create a polygon from the bounding box
-    auto pedestrian = boundingBox3dToPolygon3d(pedestrianBBox);
+    auto pedestrian = utils::boundingBox3dToPolygon3d(pedestrianBBox);
     lanelet::RuleParameterMap rpm = {{lanelet::RoleNameString::Refers, {pedestrian}}};
     lanelet::AttributeMap am;
     auto data = std::make_shared<lanelet::RegulatoryElementData>(lanelet::utils::getId(), std::move(rpm), am);
@@ -36,7 +16,7 @@ std::shared_ptr<PedestrianRegElem> PedestrianRegElem::make(const lanelet::Boundi
 
 void PedestrianRegElem::updatePedestrian(const lanelet::BoundingBox3d& pedestrianBBox){
     // Create a polygon from the bounding box
-    auto pedestrian = boundingBox3dToPolygon3d(pedestrianBBox);
+    auto pedestrian = utils::boundingBox3dToPolygon3d(pedestrianBBox);
     parameters()[lanelet::RoleName::Refers].clear();
     parameters()[lanelet::RoleName::Refers].emplace_back(pedestrian);
 }
