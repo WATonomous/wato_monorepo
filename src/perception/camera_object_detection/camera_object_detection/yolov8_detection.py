c054c5fa7a3cf88b6dcd57c2391d38c024392630
Arfan12630 <arfanrahman464@gmail.com>
2024-11-05 21:19:59 +0000

    Completed batching and creating tensrt engine

--- src/perception/camera_object_detection/camera_object_detection/yolov8_detection.py
+++ src/perception/camera_object_detection/camera_object_detection/yolov8_detection.py
@@ -1,14 +1,14 @@
 import rclpy
 from rclpy.node import Node
 import os
-
+from message_filters import Subscriber, TimeSynchronizer, ApproximateTimeSynchronizer
 from sensor_msgs.msg import Image, CompressedImage
 from vision_msgs.msg import (
     ObjectHypothesisWithPose,
     Detection2D,
     Detection2DArray,
 )
-
+from ultralytics import YOLO
 from ultralytics.nn.autobackend import AutoBackend
 from ultralytics.data.augment import LetterBox, CenterCrop
 from ultralytics.utils.ops import non_max_suppression
@@ -22,6 +22,8 @@ import numpy as np
 import time
 
 import torch
+import onnx
+import tensorrt as trt
 
 
 class CameraDetectionNode(Node):
@@ -30,21 +32,65 @@ class CameraDetectionNode(Node):
         torch.zeros(1).cuda()
 
         super().__init__("camera_object_detection_node")
-        self.get_logger().info("Creating camera detection node...")
+        self.get_logger().info("Creating batched camera detection node...")
+        
 
+        #Eve config params 
         self.declare_parameter("camera_topic", "/camera/right/image_color")
+        self.declare_parameter("left_camera_topic", "/camera/left/image_color")
+        self.declare_parameter("center_camera_topic", "/camera/center/image_color")
+
+        #Nuscenes 
+        self.declare_parameter("front_center_camera_topic", "/CAM_FRONT/image_rect_compressed")
+        self.declare_parameter("front_right_camera_topic", "/CAM_FRONT_RIGHT/image_rect_compressed")
+        self.declare_parameter("front_left_camera_topic", "/CAM_FRONT_LEFT/image_rect_compressed")
+
+        self.declare_parameter("back_center_camera_topic", "/CAM_BACK/image_rect_compressed")
+        self.declare_parameter("back_right_camera_topic", "/CAM_BACK_RIGHT/image_rect_compressed")
+        self.declare_parameter("back_left_camera_topic", "/CAM_BACK_LEFT/image_rect_compressed")
+
+        self.declare_parameter("onnx_model_path", "/perception_models/yolov8m.onnx")
+        self.declare_parameter("tensorRT_model_path", "/perception_models/yolov8m.engine")
         self.declare_parameter("publish_vis_topic", "/annotated_img")
         self.declare_parameter("publish_detection_topic", "/detections")
         self.declare_parameter("model_path", "/perception_models/yolov8m.pt")
+       
         self.declare_parameter("image_size", 1024)
         self.declare_parameter("compressed", False)
         self.declare_parameter("crop_mode", "LetterBox")
         self.declare_parameter("save_detections", False)
+        
+        #Declare batch topic 
+        self.declare_parameter("batch_inference_topic", "/batched_camera_message")
+       
+       
+        
+       #Batch inference topic 
+        self.batch_inference_topic = self.get_parameter("batch_inference_topic").value
+        self.onnx_model_path = self.get_parameter("onnx_model_path").value
+        self.tensorRT_model_path = self.get_parameter("tensorRT_model_path").value
 
+        #Camera topics for eve 
         self.camera_topic = self.get_parameter("camera_topic").value
+        self.left_camera_topic = self.get_parameter("left_camera_topic").value
+        self.center_camera_topic = self.get_parameter("center_camera_topic").value
+
+        #Camera topics for nuscenes 
+        self.front_center_camera_topic  = self.get_parameter("front_center_camera_topic").value
+        self.front_right_camera_topic = self.get_parameter("front_right_camera_topic").value
+        self.front_left_camera_topic = self.get_parameter("front_left_camera_topic").value
+
+        self.back_center_camera_topic = self.get_parameter("back_center_camera_topic").value
+        self.back_right_camera_topic = self.get_parameter("back_right_camera_topic").value
+        self.back_left_camera_topic  = self.get_parameter("back_left_camera_topic").value
+
+
+        #Parameter for TensorRT Model
+        
         self.publish_vis_topic = self.get_parameter("publish_vis_topic").value
         self.publish_detection_topic = self.get_parameter("publish_detection_topic").value
         self.model_path = self.get_parameter("model_path").value
+      
         self.image_size = self.get_parameter("image_size").value
         self.compressed = self.get_parameter("compressed").value
         self.crop_mode = self.get_parameter("crop_mode").value
@@ -57,6 +103,8 @@ class CameraDetectionNode(Node):
         self.line_thickness = 1
         self.half = False
 
+        
+
         self.subscription = self.create_subscription(
             Image if not self.compressed else CompressedImage,
             self.camera_topic,
@@ -67,11 +115,22 @@ class CameraDetectionNode(Node):
                 depth=10,
             ),
         )
+        #Subscription for Nuscenes
+        self.front_center_camera_subscription = Subscriber(self, CompressedImage, self.front_center_camera_topic)
+        self.front_right_camera_subscription  = Subscriber(self, CompressedImage, self.front_right_camera_topic)
+        self.front_left_camera_subscription = Subscriber(self, CompressedImage, self.front_left_camera_topic)
+        self.back_center_camera_subscription = Subscriber(self, CompressedImage, self.back_center_camera_topic)
+        self.back_right_camera_subscription = Subscriber(self, CompressedImage, self.back_right_camera_topic)
+        self.back_left_camera_subscription = Subscriber(self, CompressedImage, self.back_left_camera_topic)
+        self.ats = ApproximateTimeSynchronizer([self.front_center_camera_subscription, self.back_center_camera_subscription, self.front_right_camera_subscription, self.back_right_camera_subscription, self.front_left_camera_subscription, self.back_left_camera_subscription], queue_size=10, slop=0.1)
+        self.ats.registerCallback(self.batch_callback)
+
+
 
         self.orig_image_width = None
         self.orig_image_height = None
 
-        # set device
+        # set devicepublish_visDete
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if torch.cuda.is_available():
             self.get_logger().info("Using GPU for inference")
@@ -81,22 +140,76 @@ class CameraDetectionNode(Node):
         # CV bridge
         self.cv_bridge = CvBridge()
 
-        # load yolov8 model
+        self.logger = trt.Logger(trt.Logger.WARNING)
+        self.builder = trt.Builder(self.logger)
+        self.network = self.builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+        self.parser = trt.OnnxParser(self.network, self.logger)
+    #Commented this out because only run when building the tensorRT engine
+    #     #Reading the onnx file in the perception models directory
+    #     with open(self.onnx_model_path, "rb") as model_file:
+       
+    #      if not self.parser.parse(model_file.read()):
+            
+    #         for error in range(parser.num_errors):
+    #             print(self.parser.get_error(error))
+    #         raise RuntimeError("Failed to parse the ONNX file.")
+
+    #     #Building the tensorRT engine for inferencing 
+    #     self.config = self.builder.create_builder_config()
+    #     self.total_memory = torch.cuda.get_device_properties(self.device).total_memory
+    #     self.config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, self.total_memory)
+    #     self.engine_bytes = self.builder.build_serialized_network(self.network, self.config)
+    #     assert self.engine_bytes is not None, "Failed to create engine"
+    #     self.get_logger().info("BUILT THE ENGINE ")
+       
+    #    #Writing to the perception models directory
+    #    # Getting bug on these lines 
+    #     with open(self.tensorRT_model_path, "wb") as f:
+    #         self.get_logger().info("WRITINTG THE ENGINE ")
+    #         f.write(self.engine_bytes)
+
         self.model = AutoBackend(self.model_path, device=self.device, dnn=False, fp16=False)
-
         self.names = self.model.module.names if hasattr(self.model, "module") else self.model.names
-
         self.stride = int(self.model.stride)
 
         # setup vis publishers
+        self.batched_camera_message_publisher  = self.create_publisher(Image,self.batch_inference_topic, 10)
         self.vis_publisher = self.create_publisher(Image, self.publish_vis_topic, 10)
         self.detection_publisher = self.create_publisher(
             Detection2DArray, self.publish_detection_topic, 10
         )
-
         self.get_logger().info(
             f"Successfully created node listening on camera topic: {self.camera_topic}..."
         )
+
+    def batch_callback(self,msg1,msg2,msg3, msg4,msg5,msg6):
+        image_list =  [msg1,msg2,msg3,msg4,msg5,msg6]
+        batched_list = []
+        for msg in image_list:  
+            # Need to convert to numpy array and then make it have 3 color channels 
+            numpy_array = np.frombuffer(msg.data, np.uint8)
+            compressedImage  = cv2.imdecode(numpy_array, cv2.IMREAD_COLOR)
+            resized_compressedImage = cv2.resize(compressedImage,(1920, 1080))
+            preprocessedImage = self.batch_convert_to_tensor(resized_compressedImage)
+            batched_list.append(preprocessedImage)
+            # self.get_logger().info(f"Resized Image Shape:{resized_compressedImage.shape}")
+           
+        
+        batch_tensor = torch.stack(batched_list)
+        self.get_logger().info(f"batch tensor shape: {batch_tensor.shape}")
+
+        #Now need to pass it to yolov8 tensorRT model
+        # predictions = self.model(batch_tensor)
+       
+        
+
+    #Converting to tensor for batching
+    def batch_convert_to_tensor(self, cv_image):
+        img  = cv_image.transpose(2,0,1)
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        return img
 
     def crop_image(self, cv_image):
         if self.crop_mode == "LetterBox":
@@ -157,7 +270,7 @@ class CameraDetectionNode(Node):
         img = self.crop_image(cv_image)
 
         # Convert
-        img = img.transpose(2, 0, 1)
+        img = cv_image.transpose(2, 0, 1)
 
         # Further conversion
         img = torch.from_numpy(img).to(self.device)
@@ -255,7 +368,7 @@ class CameraDetectionNode(Node):
             pred = self.model(img)
 
             # nms function used same as yolov8 detect.py
-            pred = non_max_suppression(pred)
+            pred = non_max_suppression(pred) #Eliminates overlapping bounding boxes
             detections = []
             for i, det in enumerate(pred):  # per image
                 if len(det):
