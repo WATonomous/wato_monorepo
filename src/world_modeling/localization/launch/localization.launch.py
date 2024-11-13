9aadefe1518366c029565eccaa155fa76a1fd97e
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-11-13 03:48:18 +0000

    localization launch file

--- src/world_modeling/localization/launch/localization.launch.py
+++ src/world_modeling/localization/launch/localization.launch.py
@@ -9,7 +9,22 @@ import os
 
 def generate_launch_description():
     """Launch localization node."""
+    config_file = LaunchConfiguration('config', default_value='./config/config.yaml')   #getting config we defined in config.yaml
+
 
     return LaunchDescription([
-      
+        # Declare the launch argument
+        DeclareLaunchArgument(
+            'config',
+            default_value = config_file,
+        ),
+        
+        Node(
+            package='robot_localization',
+            executable='ekf_localization_node',
+            name='ekf_localization_node',
+            parameters=[config_file],
+            output='screen'
+        )
     ])
+
