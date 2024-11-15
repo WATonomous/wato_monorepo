95b046cd99aa9d6c6dd0723afd8024a81c72cf9e
VishGit1234 <vishalschool1234@gmail.com>
2024-11-15 23:23:16 +0000

    add navsat transform node

--- src/world_modeling/localization/launch/localization.launch.py
+++ src/world_modeling/localization/launch/localization.launch.py
@@ -9,22 +9,41 @@ import os
 
 def generate_launch_description():
     """Launch localization node."""
-    config_file = LaunchConfiguration('config', default_value='./config/config.yaml')   #getting config we defined in config.yaml
+    config_file_path = os.path.join(
+        get_package_share_directory('localization'),
+        'config',
+        'config.yaml'
+    )
 
 
     return LaunchDescription([
         # Declare the launch argument
         DeclareLaunchArgument(
             'config',
-            default_value = config_file,
+            default_value = config_file_path,
         ),
         
         Node(
             package='robot_localization',
-            executable='ekf_localization_node',
-            name='ekf_localization_node',
-            parameters=[config_file],
+            executable='ekf_node',
+            name='ekf_filter_node_global',
+            parameters=[config_file_path],
+            remappings=[("odometry/filtered", "odometry/filtered")],
             output='screen'
+        ),
+        Node(
+            package='robot_localization',
+            executable='navsat_transform_node',
+            name='navsat_transform',
+            parameters=[config_file_path],
+            output='screen',
+            remappings=[
+                ("imu/data", "carla/ego/imu"),
+                ("gps/fix", "carla/ego/gnss"),
+                ("gps/filtered", "gps/filtered"),
+                ("odometry/gps", "odometry/gps"),
+                ("odometry/filtered", "odometry/filtered"),
+            ],
         )
     ])
 
