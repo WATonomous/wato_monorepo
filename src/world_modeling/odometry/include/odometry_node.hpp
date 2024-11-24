0d2ffb7b468a57e79ad732392a3ee7e8fa2134d1
Sunil Rathagirishnan <srathagi@uwaterloo.ca>
2024-11-24 02:47:27 +0000

    odom node hpp

--- src/world_modeling/odometry/include/odometry_node.hpp
+++ src/world_modeling/odometry/include/odometry_node.hpp
@@ -3,48 +3,40 @@
 
 #include "rclcpp/rclcpp.hpp"
 
-#include "sample_msgs/msg/filtered.hpp"
-#include "sample_msgs/msg/filtered_array.hpp"
-#include "sample_msgs/msg/unfiltered.hpp"
-
+#include <nav_msgs/Odometry.hpp>
 #include "transformer_core.hpp"
+#include "odometry_core.hpp"
 
-/**
- * Implementation of a ROS2 node that converts unfiltered messages to filtered_array
- * messages.
- *
- * Listens to the "unfiltered" topic and filters out data with invalid fields
- * and odd timestamps. Once the node collects BUFFER_CAPACITY messages it packs
- * the processed messages into an array and publishes it to the "filtered" topic.
- */
-class TransformerNode : public rclcpp::Node {
- public:
-  // Configure pubsub nodes to keep last 20 messages.
-  // https://docs.ros.org/en/foxy/Concepts/About-Quality-of-Service-Settings.html
-  static constexpr int ADVERTISING_FREQ = 20;
+/*
+Node to take steering wheel and velocity of vehicle to track local odom
+Using the ackermanse steering model for odometry
 
-  /**
-   * Transformer node constructor.
-   */
-  TransformerNode();
+Listen to data from CAN topics and then update nav/odometry/pose message
 
- private:
-  /**
-   * A ROS2 subscription node callback used to process raw data from the
-   * "unfiltered" topic and publish to the "filtered" topic.
-   *
-   * @param msg a raw message from the "unfiltered" topic
-   */
-  void unfiltered_callback(const sample_msgs::msg::Unfiltered::SharedPtr msg);
+*/
 
-  // ROS2 subscriber listening to the unfiltered topic.
-  rclcpp::Subscription<sample_msgs::msg::Unfiltered>::SharedPtr raw_sub_;
+class OdometryNode : public rclcpp::Node {
+  public:
+    // Configure pubsub nodes to keep last 20 messages.
+    // https://docs.ros.org/en/foxy/Concepts/About-Quality-of-Service-Settings.html
+    static constexpr int ADVERTISING_FREQ = 20;
 
-  // ROS2 publisher sending processed messages to the filtered topic.
-  rclcpp::Publisher<sample_msgs::msg::FilteredArray>::SharedPtr transform_pub_;
+    //Odometry node constructor 
+    OdometryNode();
 
-  // Object that handles data processing and validation.
-  samples::TransformerCore transformer_;
-};
+  private:
+    //where the publisher,subscriber and callback go
+    //Add the Odometry core 
+    OdometryCore odometry_;
+    
+    //CURRENT ABSTRACTION (CAN MESSAGES)
+    rclcpp::Subscription<CAN_msgs::msg::recievedMsgs>::SharedPtr _subscriber;
+
+
+    rclcpp::Publisher<nav_msgs::Odometry>::SharedPtr _publisher;
+
+    void subscription_callback(const CANmsgs::msg::recievedMsgs::SharedPtr raw_CAN_Data);
+  
+}
 
 #endif  // TRANSFORMER_NODE_HPP_
