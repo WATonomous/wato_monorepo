617fb15f9dd941490e279f83df38fd2ffbea466f
Eddy Zhou <edward.zhzh@gmail.com>
2025-01-20 18:19:23 -0800

    some restructuring, alot of things broke most likely

--- src/interfacing/sensor_interfacing/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rosinstall_generator ros2_socketcan > ros2_socketcan.rosinstall
-rosinstall_generator can_msgs > can_msgs.rosinstall
-rosinstall . ros2_socketcan.rosinstall
-rosinstall . can_msgs.rosinstall
