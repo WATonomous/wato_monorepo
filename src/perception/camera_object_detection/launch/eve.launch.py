a489b2c32e0cb2ddff67224f57332bddf22cabf9
Mark Chiu <miniapple8888@gmail.com>
2025-01-20 20:38:51 -0500

    Camera detections using Single Node (#132)
    * working on combined
    
    * added model iterator
    
    * Remove watod changes
    
    * Trying to pass models in
    
    * Combine models for camera detections
    
    * linted
    
    ---------
    
    Co-authored-by: Momin Chaudhry <hellomomins@yahoo.com>
    Co-authored-by: Dan Ryan Huynh <89366190+danielrhuynh@users.noreply.github.com>
    Co-authored-by: Dan Huynh <danielryanh7@gmail.com>

--- src/perception/camera_object_detection/launch/eve.launch.py
+++ src/perception/camera_object_detection/launch/eve.launch.py
@@ -3,55 +3,43 @@ from launch.substitutions import LaunchConfiguration
 from launch.conditions import LaunchConfigurationEquals
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
-    launch_traffic_light = LaunchConfiguration("launch_traffic_light", default=True)
-    launch_traffic_light_arg = DeclareLaunchArgument(
-        "launch_traffic_light",
-        default_value=launch_traffic_light,
-        description="Launch traffic light detection",
-    )
-    launch_traffic_signs = LaunchConfiguration("launch_traffic_signs", default=True)
-    launch_traffic_signs_arg = DeclareLaunchArgument(
-        "launch_traffic_signs",
-        default_value=launch_traffic_signs,
-        description="Launch traffic signs detection",
+    config = os.path.join(
+        get_package_share_directory("camera_object_detection"),
+        "config",
+        "combined_config.yaml"
     )
 
-    launch_args = [launch_traffic_light_arg, launch_traffic_signs_arg]
-
-    camera_object_detection_launch_include_dir = os.path.join(
-        get_package_share_directory("camera_object_detection"), "launch", "include"
+    left_combined_detection_node = Node(
+        package="camera_object_detection",
+        executable="camera_object_detection_node",
+        name="left_combined_detection_node",
+        parameters=[config],
     )
 
-    pretrained_yolov8_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [camera_object_detection_launch_include_dir, "/pretrained_yolov8.launch.py"]
-        ),
+    center_combined_detection_node = Node(
+        package="camera_object_detection",
+        executable="camera_object_detection_node",
+        name="center_combined_detection_node",
+        parameters=[config],
     )
 
-    traffic_light_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [camera_object_detection_launch_include_dir, "/traffic_light.launch.py"]
-        ),
-        condition=LaunchConfigurationEquals("launch_traffic_light", "True"),
-    )
-
-    traffic_signs_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [camera_object_detection_launch_include_dir, "/traffic_signs.launch.py"]
-        ),
-        condition=LaunchConfigurationEquals("launch_traffic_signs", "True"),
+    right_combined_detection_node = Node(
+        package="camera_object_detection",
+        executable="camera_object_detection_node",
+        name="right_combined_detection_node",
+        parameters=[config],
     )
 
     return LaunchDescription(
-        launch_args
-        + [
-            pretrained_yolov8_launch,
-            traffic_light_launch,
-            traffic_signs_launch,
+        [
+            left_combined_detection_node,
+            center_combined_detection_node,
+            right_combined_detection_node
         ]
     )
