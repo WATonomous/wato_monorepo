18b7f9336e5f3d4cf9cf9de6dc38e8d38b27a3cd
Eddy Zhou <edward.zhzh@gmail.com>
2025-01-20 18:20:15 -0800

    Revert "temp - testing stuff"
    This reverts commit fe8b2e2a66d6333c49636f56b31f5d5c68132313.

--- src/perception/radar_object_detection/radar_vis/src/radar_vis_node.cpp
+++ src/perception/radar_object_detection/radar_vis/src/radar_vis_node.cpp
@@ -4,18 +4,14 @@
 #include "radar_vis_node.hpp"
 
 RadarVisNode::RadarVisNode() : Node("radar_vis") {
-  RCLCPP_INFO(this->get_logger(), "creating");
-
   raw_sub_ = this->create_subscription<radar_msgs::msg::RadarPacket>(
-      "/radar_packet", 1,
+      "radar_packet", 1,
       std::bind(&RadarVisNode::process_radar_data_callback, this, std::placeholders::_1));
-  raw_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/visualization", 20);
-  marker_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("/radar_markers", 20);
+  raw_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("visualization", 20);
+  marker_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("radar_markers", 20);
 }
 
 void RadarVisNode::process_radar_data_callback(const radar_msgs::msg::RadarPacket::SharedPtr msg) {
-  RCLCPP_INFO(this->get_logger(), "publishing");
-
   sensor_msgs::msg::PointCloud2 publish_packet_point_cloud;
   publish_packet_point_cloud = packet_converter_.convert_packet_to_pointcloud(msg);
   raw_pub_->publish(publish_packet_point_cloud);
