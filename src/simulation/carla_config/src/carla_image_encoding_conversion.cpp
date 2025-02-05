949debd875af44b07e44e17c63089248af5706ce
WATonomousAdmin <admin@watonomous.ca>
2025-02-05 06:58:36 +0000

    Fix code style issues with clang_format

--- src/simulation/carla_config/src/carla_image_encoding_conversion.cpp
+++ src/simulation/carla_config/src/carla_image_encoding_conversion.cpp
@@ -3,10 +3,8 @@
 using std::placeholders::_1;
 
 class Image_Encoding_Conversion : public rclcpp::Node {
-
  public:
   Image_Encoding_Conversion() : Node("image_encoding_conversion_node") {
-
     // Wait for service
     auto parameters_client =
         std::make_shared<rclcpp::SyncParametersClient>(this, "set_initial_pose");
@@ -27,20 +25,25 @@ class Image_Encoding_Conversion : public rclcpp::Node {
     } while (role_name == "");
 
     // Declare Parameters
-    this->declare_parameter("camera_out_center_topic", "/carla/" + role_name + "/rgb_center/image_bgr");
-    this->declare_parameter("camera_out_right_topic", "/carla/" + role_name + "/rgb_right/image_bgr");
+    this->declare_parameter("camera_out_center_topic",
+                            "/carla/" + role_name + "/rgb_center/image_bgr");
+    this->declare_parameter("camera_out_right_topic",
+                            "/carla/" + role_name + "/rgb_right/image_bgr");
     this->declare_parameter("camera_out_left_topic", "/carla/" + role_name + "/rgb_left/image_bgr");
 
     // Get Parameters
-    std::string camera_out_center_topic = this->get_parameter("camera_out_center_topic").as_string();
+    std::string camera_out_center_topic =
+        this->get_parameter("camera_out_center_topic").as_string();
     std::string camera_out_right_topic = this->get_parameter("camera_out_right_topic").as_string();
     std::string camera_out_left_topic = this->get_parameter("camera_out_left_topic").as_string();
 
-    this->imagePubCenter = this->create_publisher<sensor_msgs::msg::Image>(camera_out_center_topic, 10);
+    this->imagePubCenter =
+        this->create_publisher<sensor_msgs::msg::Image>(camera_out_center_topic, 10);
     imageSubCenter = this->create_subscription<sensor_msgs::msg::Image>(
         "/carla/" + role_name + "/rgb_center/image", 10,
         std::bind(&Image_Encoding_Conversion::update_image_center, this, _1));
-    this->imagePubRight = this->create_publisher<sensor_msgs::msg::Image>(camera_out_right_topic, 10);
+    this->imagePubRight =
+        this->create_publisher<sensor_msgs::msg::Image>(camera_out_right_topic, 10);
     imageSubRight = this->create_subscription<sensor_msgs::msg::Image>(
         "/carla/" + role_name + "/rgb_right/image", 10,
         std::bind(&Image_Encoding_Conversion::update_image_right, this, _1));
@@ -58,25 +61,26 @@ class Image_Encoding_Conversion : public rclcpp::Node {
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr imagePubLeft;
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr imageSubLeft;
 
-  void update_image_center(sensor_msgs::msg::Image::SharedPtr image){
+  void update_image_center(sensor_msgs::msg::Image::SharedPtr image) {
     sensor_msgs::msg::Image updatedImage;
     update_image(updatedImage, image);
     imagePubCenter->publish(updatedImage);
   }
 
-  void update_image_right(sensor_msgs::msg::Image::SharedPtr image){
+  void update_image_right(sensor_msgs::msg::Image::SharedPtr image) {
     sensor_msgs::msg::Image updatedImage;
     update_image(updatedImage, image);
     imagePubRight->publish(updatedImage);
   }
 
-  void update_image_left(sensor_msgs::msg::Image::SharedPtr image){
+  void update_image_left(sensor_msgs::msg::Image::SharedPtr image) {
     sensor_msgs::msg::Image updatedImage;
     update_image(updatedImage, image);
     imagePubLeft->publish(updatedImage);
   }
 
-  void update_image(sensor_msgs::msg::Image &updatedImage, sensor_msgs::msg::Image::SharedPtr image){
+  void update_image(sensor_msgs::msg::Image &updatedImage,
+                    sensor_msgs::msg::Image::SharedPtr image) {
     updatedImage.header = image->header;
     updatedImage.height = image->height;
     updatedImage.width = image->width;
@@ -85,7 +89,7 @@ class Image_Encoding_Conversion : public rclcpp::Node {
     updatedImage.step = (image->step * 3) / 4;
     updatedImage.data = std::vector<unsigned char>();
 
-    for (size_t i = 0; i < image->data.size(); i += 4){
+    for (size_t i = 0; i < image->data.size(); i += 4) {
       // Take only BGR values (skip Alpha value)
       updatedImage.data.push_back(image->data[i + 0]);
       updatedImage.data.push_back(image->data[i + 1]);
