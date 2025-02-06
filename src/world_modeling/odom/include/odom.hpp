894ef46db7f2470f6e3e87b373efd9898990a7c3
hassaan141 <hassaanfarooqi2000@gmail.com>
2025-02-06 14:57:55 +0000

    requested-changes

---
+++ src/world_modeling/odom/include/odom.hpp
@@ -0,0 +1,42 @@
+#ifndef ODOM_VALUES
+#define ODOM_VALUES
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include<rclcpp/rclcpp.hpp>
+#include<std_msgs/msg/float64.hpp>
+#include<std_msgs/msg/string.hpp>
+
+//not sure about the one below
+#include <nav_msgs/msg/odometry.hpp>
+
+using namespace std::chrono_literals;
+
+class WheelOdometry : public rclcpp::Node{
+
+public:
+  WheelOdometry();
+
+private:
+  void defineVariables(const std_msgs::msg::Float64::SharedPtr msg, const std::string &source);
+  void bicycleModel();
+
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr leftrear_wheel_motor_encoder;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr rightrear_wheel_motor_encoder;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr steering_angle;
+
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
+
+  double left_wheel_speed;
+  double right_wheel_speed;
+  double steering_angle;
+  double x_;
+  double y_;
+  double theta_;
+  rclcpp::Time previous_time_;
+  const double WHEEL_BASE = 2.65;
+};
+
+#endif
