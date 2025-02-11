106ba6071757905678effc6bc3f09d90ea23ca9e
WATonomousAdmin <admin@watonomous.ca>
2025-02-11 03:22:46 +0000

    Fix code style issues with clang_format

--- src/world_modeling/localization/src/odom_mock_data.cpp
+++ src/world_modeling/localization/src/odom_mock_data.cpp
@@ -2,15 +2,13 @@
 
 OdomMockData::OdomMockData() : Node("odom_mock_data") {
   publisher_left_wheel__ = this->create_publisher<std_msgs::msg::Float64>("left_motor_encoder", 10);
-  publisher_right_wheel_ = this->create_publisher<std_msgs::msg::Float64>("right_motor_encoder", 10);
-  publisher_steering_angle_ =
-      this->create_publisher<std_msgs::msg::Float64>("steering_angle", 10);
+  publisher_right_wheel_ =
+      this->create_publisher<std_msgs::msg::Float64>("right_motor_encoder", 10);
+  publisher_steering_angle_ = this->create_publisher<std_msgs::msg::Float64>("steering_angle", 10);
 
   // publisher called every 1 second in meters
-  timer_right_ =
-      this->create_wall_timer(1000ms, std::bind(&OdomMockData::RandomLeftValues, this));
-  timer_left_ =
-      this->create_wall_timer(1000ms, std::bind(&OdomMockData::RandomRightValues, this));
+  timer_right_ = this->create_wall_timer(1000ms, std::bind(&OdomMockData::RandomLeftValues, this));
+  timer_left_ = this->create_wall_timer(1000ms, std::bind(&OdomMockData::RandomRightValues, this));
   // publisher will fake turn values every 12 seconds
   timer_steering_ =
       this->create_wall_timer(12000ms, std::bind(&OdomMockData::RandomSteeringValues, this));
