9ee00515706f70a92fb0fad27b6b3a2e0a338d29
VishGit1234 <vishalschool1234@gmail.com>
2025-02-11 03:21:57 +0000

    move odom to localization package

--- src/world_modeling/localization/src/odom.cpp
+++ src/world_modeling/localization/src/odom.cpp
@@ -3,32 +3,29 @@
 WheelOdometry::WheelOdometry()
     : Node("sensor_subscriber"), x_(0.0), y_(0.0), theta_(0.0), previous_time_(this->now()) {
   leftrear_wheel_motor_encoder = this->create_subscription<std_msgs::msg::Float64>(
-      "/motor_encoder_left", 10,
-      std::bind(&WheelOdometry::defineVariables, this, std::placeholders::_1, "left_encoder"));
+      "/left_motor_encoder", 10, 
+      [this](const std_msgs::msg::Float64::SharedPtr msg) {
+        left_wheel_speed = msg->data;
+      });
 
   rightrear_wheel_motor_encoder = this->create_subscription<std_msgs::msg::Float64>(
-      "/motor_encoder_right", 10,
-      std::bind(&WheelOdometry::defineVariables, this, std::placeholders::_1, "right_encoder"));
+      "/right_motor_encoder", 10,
+      [this](const std_msgs::msg::Float64::SharedPtr msg) {
+        right_wheel_speed = msg->data;
+      });
 
-  steering_angle = this->create_subscription<std_msgs::msg::Float64>(
+  steering_angle_subscription = this->create_subscription<std_msgs::msg::Float64>(
       "/steering_angle", 10,
-      std::bind(&WheelOdometry::defineVariables, this, std::placeholders::_1, "steering_angle"));
+      [this](const std_msgs::msg::Float64::SharedPtr msg) {
+        steering_angle = msg->data;
+      });
 
-  publisher_ = this->create_publisher<std_msgs::msg::Float64>("/bicycle_model_output", 10);
+  publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("/bicycle_model_output", 10);
+  
+  timer_ = this->create_wall_timer(1000ms, std::bind(&WheelOdometry::bicycleModel, this));
 }
 
-void WheelOdometry::defineVariables(const std_msgs::msg::Float64::SharedPtr msg,
-                                    const std::string &source) {
-  if (source == "left_encoder") {
-    left_wheel_speed = msg->data;
-  } else if (source == "right_encoder") {
-    right_wheel_speed = msg->data;
-  } else if (source == "steering_angle") {
-    steering_angle = msg->data;
-  }
-}
-
-void WheelOdometery::bicycleModel() {
+void WheelOdometry::bicycleModel() {
   double linear_velocity = (left_wheel_speed + right_wheel_speed) / 2.0;
   double angular_velocity = linear_velocity * tan(steering_angle) / WHEEL_BASE;
 
@@ -53,7 +50,7 @@ void WheelOdometery::bicycleModel() {
   odom_message.twist.twist.linear.x = linear_velocity;
   odom_message.twist.twist.angular.z = angular_velocity;
 
-  RCLCPP_INFO(this->get_logger(), "Publishing: x=%.2f, y=%.2f, theta=%.2f", x_, y_, theta_);
+  RCLCPP_DEBUG(this->get_logger(), "Publishing: x=%.2f, y=%.2f, theta=%.2f", x_, y_, theta_);
   publisher_->publish(odom_message);
 }
 
