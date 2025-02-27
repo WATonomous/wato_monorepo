423c14b970f52a482eb15efe21995e6ece1c8b4f
Tei06 <brianzheng111@gmail.com>
2025-02-27 19:27:23 +0000

    remove floor using RANSAC

--- src/perception/tracking/lidar_overlay/include/projection_utils.hpp
+++ src/perception/tracking/lidar_overlay/include/projection_utils.hpp
@@ -1,17 +1,18 @@
 #ifndef PROJECTION_UTILS_HPP
 #define PROJECTION_UTILS_HPP
 
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <random>
-#include <algorithm>
-#include <limits>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/sample_consensus/ransac.h>
-#include <pcl/sample_consensus/sac_model_plane.h>
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+class ProjectionUtils {
+public:
+    ProjectionUtils() {}
+    static void removeGroundPlane(PointCloud::Ptr& cloud);
+
+private:
+   
+};
 
 #endif
