50440b5f14bfa15b4a1dcc46ce989e05e4bdae57
Tei06 <brianzheng111@gmail.com>
2025-02-28 06:39:30 +0000

    added 2d detections with depth distance

--- src/perception/tracking/lidar_overlay/include/lidar_overlay.hpp
+++ src/perception/tracking/lidar_overlay/include/lidar_overlay.hpp
@@ -1,26 +1,61 @@
-#ifndef LIDAR_OVERLAY
-#define LIDAR_OVERLAY
+#ifndef LIDAR_OVERLAY_HPP
+#define LIDAR_OVERLAY_HPP
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
-#include <geometry_msgs/msg/point.hpp>
 #include <vision_msgs/msg/detection2_d_array.hpp>
-
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
-#include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <tf2/transform_datatypes.h>
-#include <pcl/common/transforms.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
-#include <geometry_msgs/msg/transform_stamped.hpp>
-#include <optional>
+#include "projection_utils.hpp" 
 
-#include "projection_utils.hpp"
+class LidarImageOverlay : public rclcpp::Node {
+    public:
+        LidarImageOverlay();
 
-#endif
+    private:
+        // IMAGE -----------------------------------------------------------------------------------------------------------
+        void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
+        cv_bridge::CvImagePtr image_data_;
+        std::array<double, 12> projection_matrix_;
+
+        void cameraInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
+
+        // LIDAR -----------------------------------------------------------------------------------------------------------
+        void lidarCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+        sensor_msgs::msg::PointCloud2 latest_lidar_msg_;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_point_cloud_;
+
+        // DETECTIONS ------------------------------------------------------------------------------------------------------
+        void detsCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg);
+
+        // PROJECTION ------------------------------------------------------------------------------------------------------
+        std::optional<cv::Point2d> projectLidarToCamera(
+            const geometry_msgs::msg::TransformStamped& transform,
+            const std::array<double, 12>& p,
+            const pcl::PointXYZ& pt,
+            int image_width = 1600,
+            int image_height = 900);
+
+        // SUBSCRIBERS -----------------------------------------------------------------------------------------------------
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_sub_;
+        rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_sub_;
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+        rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr dets_sub_;
+
+        std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+        std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
+        // PUBLISHERS ------------------------------------------------------------------------------------------------------
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
+};
+
+#endif 
