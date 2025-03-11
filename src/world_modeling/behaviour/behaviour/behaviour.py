a51f0802202d9f43b7a13e0cab6eb212660081e4
Anjanan Thirumahan <yoanjanan.thirumahan@gmail.com>
2025-03-11 18:14:09 +0000

    implemented bt spin_once and aggregation - not tested yet

--- src/world_modeling/behaviour/behaviour/behaviour.py
+++ src/world_modeling/behaviour/behaviour/behaviour.py
@@ -5,18 +5,18 @@ from world_modeling_msgs.srv import BehaviourTreeInfo
 from behaviour.root import Root
 import py_trees
 
-class BehaviourNode(Root, Node):
+class BehaviourNode(Node):
     def __init__ (self):
         # Setup node 
-        Node.__init__(self, 'behaviour')
+        super().__init__(self, 'behaviour')
         self.client = self.create_client(BehaviourTreeInfo, 'behaviour_tree_info')
         while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service not available, waiting again...')
         self.request = BehaviourTreeInfo.Request()
-
+        
         # Setup behaviour tree
-        Root.__init__(self)
-        setup_status = self.setup()   
+        self.root = Root()
+        setup_status = self.root.setup()   
         if setup_status == py_trees.common.Status.SUCCESS:
             print("Setup for behaviour tree completed successfully.")
         else:
@@ -33,14 +33,30 @@ class BehaviourNode(Root, Node):
     def query_state(self, _):
         response = self.send_request()
         self.get_logger().debug(f'Received response: {response}')
+        
+        #update blackboard
+        self.root.blackboard.current_pos = response.current_pos
+        self.root.blackboard.current_pos = response.goal_pos
+        self.root.blackboard.route_list = response.route_list
 
 def main(args=None):
     rclpy.init(args=args)
     behaviour_node = BehaviourNode()
 
     # This is blocking so always keep it 
-    py_trees.trees.BehaviourTree(behaviour_node).tick_tock(period_ms=1000, pre_tick_handler=behaviour_node.query_state) # 1000 should be made a parameter
-
+    try: 
+        while rclpy.ok():
+            rclpy.spin_once(behaviour_node)
+            behaviour_tree.tick() 
+    except KeyboardInterrupt:
+        pass   
+    finally:
+        rclpy.shutdown()
+    #py_trees.trees.BehaviourTree(behaviour_node).tick_tock(period_ms=1000, pre_tick_handler=behaviour_node.query_state) # 1000 should be made a parameter
+    
+    #while loop  
+        #RCLPY spin once
+        #RCLPY tick once
     rclpy.shutdown()
 
 if __name__ == '__main__':
