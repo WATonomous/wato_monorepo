a9527fa4f7b154e11931d10105fabd2dbff4c7b7
Mike Katsnelson <mike.katsnelson@gmail.com>
2025-03-11 03:15:24 +0000

    fix some naming, add more randomality to control samples generation

--- src/action/model_predictive_control/model_predictive_path_integral/mppi_core.py
+++ src/action/model_predictive_control/model_predictive_path_integral/mppi_core.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 class mppi_core:
-    def __init__(self):
-        self.numPaths = 500 # number of paths
-        self.numTimeSteps = 50 #number of time steps
-        self.controlSequence = np.zeros((2,self.numTimeSteps))
-        self.PrevControlSequence = np.zeros((2,self.numTimeSteps))
+    def __init__(self): 
+        self.numSamples = 500 # number of samples/path to generate at each timesetep
+        self.numTimeSteps = 50 # number of time steps
+        
+        self.prevControlSequence = np.zeros((2,self.numTimeSteps)) # input
+        self.outControlSequence = np.zeros((2,self.numTimeSteps)) # output
+        
+        # 
         self.controlPaths = np.zeros((2,self.numTimeSteps,self.numPaths))
         self.corresponding_dynamics = np.zeros((4,self.numTimeSteps,self.numPaths))
 
@@ -18,15 +21,22 @@ class mppi_core:
         self.std_dev_accel = 0
         self.std_dev_theta = 0
         pass
-    def generate_controls(self): #random control sequences
+    
+    def generate_control_samples(self): # random control sequences
         
         if self.initialRun:
             self.PrevControlSequence[1:] = self.theta0
-        ThetaNoise = np.random.normal(self.theta0, self.std_dev_theta, size=(self.numTimeSteps, self.numPaths))
-        AccelNoise = np.random.normal(0, self.std_dev_accel, size=(self.numTimeSteps, self.numPaths))
+            self.initialRun = False
 
-        self.controlPaths[0,:,:] = self.PrevControlSequence[0,:np.newaxis] + ThetaNoise
-        self.controlPaths[1,:,:] = self.PrevControlSequence[1,:np.newaxis] + AccelNoise
+        ThetaNoise = np.zeros((self.numSamples, self.numTimeSteps))
+        AccelNoise = np.zeros((self.numSamples, self.numTimeSteps))
+        
+        for i in range(0, self.numSamples):
+            ThetaNoise[i, :] = np.random.normal(self.theta0, self.std_dev_theta, size=(self.numTimeSteps, self.numPaths))
+            AccelNoise[i, :] = np.random.normal(0, self.std_dev_accel, size=(self.numTimeSteps, self.numPaths))
+
+            self.controlPaths[0, i, :] = self.PrevControlSequence[0,: self.numTimeSteps] + ThetaNoise
+            self.controlPaths[1,i , :] = self.PrevControlSequence[1,: self.numTimeSteps] + AccelNoise
 
     def simulate_trajectories(self):
         
