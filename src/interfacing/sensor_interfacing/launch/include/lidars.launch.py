58fa0622566fb26498cb1e0ecef9d0d72291837e
WATonomousAdmin <admin@watonomous.ca>
2025-05-08 23:23:36 +0000

    Fix code style issues with Autopep8

--- src/interfacing/sensor_interfacing/launch/include/lidars.launch.py
+++ src/interfacing/sensor_interfacing/launch/include/lidars.launch.py
@@ -21,23 +21,25 @@ def generate_launch_description():
                                 output='both',
                                 parameters=[params])
 
-
     convert_share_dir = get_package_share_directory('velodyne_pointcloud')
-    convert_params_file = os.path.join(convert_share_dir, 'config', 'VLP32C-velodyne_transform_node-params.yaml')
+    convert_params_file = os.path.join(
+        convert_share_dir, 'config', 'VLP32C-velodyne_transform_node-params.yaml')
     with open(convert_params_file, 'r') as f:
         convert_params = yaml.safe_load(f)['velodyne_transform_node']['ros__parameters']
-    convert_params['calibration'] = os.path.join(convert_share_dir, 'params', 'VeloView-VLP-32C.yaml')
+    convert_params['calibration'] = os.path.join(
+        convert_share_dir, 'params', 'VeloView-VLP-32C.yaml')
     velodyne_transform_node = Node(package='velodyne_pointcloud',
-                                                      executable='velodyne_transform_node',
-                                                      output='both',
-                                                      parameters=[convert_params])
+                                   executable='velodyne_transform_node',
+                                   output='both',
+                                   parameters=[convert_params])
 
     laserscan_share_dir = get_package_share_directory('velodyne_laserscan')
-    laserscan_params_file = os.path.join(laserscan_share_dir, 'config', 'default-velodyne_laserscan_node-params.yaml')
+    laserscan_params_file = os.path.join(
+        laserscan_share_dir, 'config', 'default-velodyne_laserscan_node-params.yaml')
     velodyne_laserscan_node = Node(package='velodyne_laserscan',
-                                                      executable='velodyne_laserscan_node',
-                                                      output='both',
-                                                      parameters=[laserscan_params_file])
+                                   executable='velodyne_laserscan_node',
+                                   output='both',
+                                   parameters=[laserscan_params_file])
 
     return LaunchDescription([
         velodyne_driver_node,
