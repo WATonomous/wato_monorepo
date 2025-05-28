470d26c8ee9a090080c2ac99b728fc663bd6c69b
khuang9 <k279huan@uwaterloo.ca>
2025-05-28 14:36:48 -0400

    add launch file to share dir

--- src/perception/tracking/setup.py
+++ src/perception/tracking/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'tracking'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools', 'nuscenes-devkit'],
     zip_safe=True,
