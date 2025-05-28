2209fca18f45c10a7f894a9febf6200e03dca59d
khuang9 <k279huan@uwaterloo.ca>
2025-05-28 00:57:15 -0400

    update dependencies

--- src/perception/tracking/setup.py
+++ src/perception/tracking/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'nuscenes-devkit'],
     zip_safe=True,
     maintainer='kishoreyogaraj',
     maintainer_email='kishore.yogaraj@gmail.com',
