6837517274d461d08439f517ce3f041ae1a5a113
khuang9 <k279huan@uwaterloo.ca>
2025-05-28 01:06:13 -0400

    launch file

---
+++ src/perception/tracking/launch/tracking.launch.py
@@ -0,0 +1,30 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """Launch tracking node."""
+    tracking_pkg_prefix = get_package_share_directory('tracking')
+    tracking_param_file = os.path.join(tracking_pkg_prefix, 'config', 'mahalanobis.yaml')
+
+    tracking_param = DeclareLaunchArgument(
+        'tracking_param_file',
+        default_value=tracking_param_file,
+        description='Path to config file for tracking node'
+    )
+
+    tracking_node = Node(
+        package='tracking',
+        executable='tracking_node',
+        parameters=[LaunchConfiguration('tracking_param_file')],
+    )
+
+    return LaunchDescription([
+        tracking_param,
+        tracking_node
+    ])
