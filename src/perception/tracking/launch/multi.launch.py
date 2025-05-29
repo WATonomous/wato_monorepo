b95f8d6ffca1ecac0a32aabec9c0180fd5bfa7f1
khuang9 <k279huan@uwaterloo.ca>
2025-05-28 22:59:07 -0400

    configurable multi-node launch file (choose what to launch through arguments)

---
+++ src/perception/tracking/launch/multi.launch.py
@@ -0,0 +1,36 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """Launch tracking node."""
+    tracking_pkg_prefix = get_package_share_directory('tracking')
+    tracking_param_file = os.path.join(tracking_pkg_prefix, 'config', 'tracking_params.yaml')
+    
+    tracking_param = DeclareLaunchArgument(
+        'tracking_param_file',
+        default_value=tracking_param_file,
+        description='Path to config file for tracking nodes'
+    )
+
+    ld = LaunchDescription([tracking_param])
+
+    nodes = ['tracker_node', 'nusc_publisher']
+
+    for n in nodes:
+        ld.add_action(DeclareLaunchArgument(n, default_value='true'))
+        ld.add_action(Node(
+            package='tracking',
+            name=n,
+            executable=n,
+            parameters=[LaunchConfiguration('tracking_param_file')],
+            condition=IfCondition(LaunchConfiguration(n))
+        ))
+
+    return ld
