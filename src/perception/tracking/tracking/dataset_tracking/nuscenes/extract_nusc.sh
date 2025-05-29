8d08874f9b859e8abcb853b3ac0a809c4b1c46ec
khuang9 <k279huan@uwaterloo.ca>
2025-05-29 00:54:54 -0400

    stuff

--- src/perception/tracking/tracking/dataset_tracking/nuscenes/extract_nusc.sh
+++ src/perception/tracking/tracking/dataset_tracking/nuscenes/extract_nusc.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Download desired dataset .tgz file from https://www.nuscenes.org/nuscenes#download
+
 filepath=$1
 filename="$(basename ${filepath})"
 dataname="${filename%.*}"
-echo "${dataname}" > dataset_name.txt
+echo "${dataname}" > ../../../nusc_info/dataset_name.txt
 
 datadir="${dataname}_data"
 mkdir -p "${datadir}"
