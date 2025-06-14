1e415abc1aa2fd50ada5cb4446517dcad3eefded
khuang9 <k279huan@uwaterloo.ca>
2025-06-14 19:52:10 -0400

    style

--- src/perception/tracking/setup.py
+++ src/perception/tracking/setup.py
@@ -3,8 +3,9 @@ from glob import glob
 import os
 
 package_name = 'tracking'
-with open (os.path.join('nusc_info', 'nuscenes_data_path.txt'), 'w') as f:
-    f.write(os.path.join(os.path.dirname(os.path.abspath(__file__)), "tracking", "dataset_tracking", "nuscenes"))
+with open(os.path.join('nusc_info', 'nuscenes_data_path.txt'), 'w') as f:
+    f.write(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         "tracking", "dataset_tracking", "nuscenes"))
 
 setup(
     name=package_name,
@@ -14,9 +15,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
-        (os.path.join('share', package_name, 'nusc_info'), glob(os.path.join('nusc_info', '*.txt'))),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'nusc_info'),
+         glob(os.path.join('nusc_info', '*.txt'))),
     ],
     install_requires=['setuptools', 'nuscenes-devkit'],
     zip_safe=True,
