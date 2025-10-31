06e7bef3b5e97bdbcfce6daae4ad020172127f19
khuang9 <k279huan@uwaterloo.ca>
2025-10-31 03:13:30 -0400

    pre-commit changes

--- src/perception/tracking_2d/track_viz_2d/include/track_viz_2d.hpp
+++ src/perception/tracking_2d/track_viz_2d/include/track_viz_2d.hpp
@@ -1,20 +1,35 @@
+// Copyright (c) 2025-present WATonomous. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef TRACK_VIZ_2D_HPP
 #define TRACK_VIZ_2D_HPP
 
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/opencv.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <vision_msgs/msg/detection2_d_array.hpp>
-#include <tracking_2d_msgs/msg/tracking2_d_array.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <std_msgs/msg/header.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
+#include <tracking_2d_msgs/msg/tracking2_d_array.hpp>
+#include <vision_msgs/msg/detection2_d_array.hpp>
 
-
-class track_viz_2d : public rclcpp::Node {
- public:
+class track_viz_2d : public rclcpp::Node
+{
+public:
   track_viz_2d();
 
- private:
+private:
   // Callback functions
   void detectionsCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg);
   void tracksCallback(const tracking_2d_msgs::msg::Tracking2DArray::SharedPtr msg);
@@ -22,20 +37,10 @@ class track_viz_2d : public rclcpp::Node {
 
   // Helper functions
   cv::Scalar colorLookup(std::string color);
-  void drawDets(
-    cv::Mat &image,
-    const std::vector<vision_msgs::msg::Detection2D> &dets,
-    const cv::Scalar &color
-  );
+  void drawDets(cv::Mat & image, const std::vector<vision_msgs::msg::Detection2D> & dets, const cv::Scalar & color);
   void drawTracks(
-    cv::Mat &image,
-    const std::vector<tracking_2d_msgs::msg::Tracking2D> &trks,
-    const cv::Scalar &color
-  );
-  void tryDraw(
-    cv::Mat &decoded_img,
-    const tracking_2d_msgs::msg::Tracking2DArray::SharedPtr latest_trks_
-  );
+    cv::Mat & image, const std::vector<tracking_2d_msgs::msg::Tracking2D> & trks, const cv::Scalar & color);
+  void tryDraw(cv::Mat & decoded_img, const tracking_2d_msgs::msg::Tracking2DArray::SharedPtr latest_trks_);
 
   // Subscribers
   rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr dets_sub_;
--- src/perception/tracking_2d/tracking_2d/include/tracking_2d.hpp
+++ src/perception/tracking_2d/tracking_2d/include/tracking_2d.hpp
@@ -1,29 +1,43 @@
+// Copyright (c) 2025-present WATonomous. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef TRACKING_2D_HPP
 #define TRACKING_2D_HPP
 
-#include <rclcpp/rclcpp.hpp>
-#include <vision_msgs/msg/detection2_d_array.hpp>
-#include <tracking_2d_msgs/msg/tracking2_d_array.hpp>
-#include <std_msgs/msg/header.hpp>
-
 #include <ByteTrack/BYTETracker.h>
 
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/header.hpp>
+#include <tracking_2d_msgs/msg/tracking2_d_array.hpp>
+#include <vision_msgs/msg/detection2_d_array.hpp>
 
-class tracking_2d : public rclcpp::Node {
- public:
+class tracking_2d : public rclcpp::Node
+{
+public:
   tracking_2d();
 
- private:
+private:
   // Callback functions
   void detectionsCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg);
 
   // Helper functions
   std::vector<byte_track::Object> detsToObjects(const vision_msgs::msg::Detection2DArray::SharedPtr dets);
-  tracking_2d_msgs::msg::Tracking2DArray STracksToTracks(const std::vector<byte_track::BYTETracker::STrackPtr> &strk_ptrs, const std_msgs::msg::Header &header);
+  tracking_2d_msgs::msg::Tracking2DArray STracksToTracks(
+    const std::vector<byte_track::BYTETracker::STrackPtr> & strk_ptrs, const std_msgs::msg::Header & header);
 
   // Subscribers
-  rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr
-      dets_sub_;
+  rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr dets_sub_;
 
   // Publishers
   rclcpp::Publisher<tracking_2d_msgs::msg::Tracking2DArray>::SharedPtr tracked_dets_pub_;
@@ -41,7 +55,6 @@ class tracking_2d : public rclcpp::Node {
   float match_thresh_;
 
   std::unique_ptr<byte_track::BYTETracker> tracker_;
-
 };
 
 #endif
