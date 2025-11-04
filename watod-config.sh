85a07289685b47bb4ff18f92631735e3ca0c9125
Eddy Zhou <edsteredward@gmail.com>
2025-11-04 12:09:39 -0500

    changes to watod-config

--- watod-config.sh
+++ watod-config.sh
@@ -31,7 +31,7 @@
 ##	 - simulation			:	starts simulation (carla)
 
 
-export ACTIVE_MODULES="infrastructure interfacing perception world_modeling action simulation"
+# export ACTIVE_MODULES=""
 
 ################################# MODE OF OPERATION #################################
 ## Possible modes of operation when running watod.
