8afa3fdcdfafb0ed2d2887ed7b3b9f8542b80a5e
Sean Yang <sean@seanyang.me>
2025-11-04 16:04:37 -0500

    fix: remove prediction package and associated Docker configurations, update kalman filter path

--- src/world_modeling/prediction/src/kalman_filter.cpp
+++ src/world_modeling/prediction/src/kalman_filter.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "src/world_modeling/prediction/include/prediction/kalman_filter.hpp"
+#include "prediction/kalman_filter.hpp"
 
 #include <rclcpp/rclcpp.hpp>
 
