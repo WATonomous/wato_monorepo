75ba9af7b9dc6ebb6029da12a1b5e4ea50976c48
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-12-19 21:50:03 -0500

    Add multi_hikrobot package

---
+++ src/interfacing/multi_hikrobot/launch/cam.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from dataclasses import dataclass
+
+
+@dataclass
+class CameraConfig:
+    guid: str
+    frame_id: str
+    stream_name: str
+
+
+def generate_launch_description():
+    configs = [
+        CameraConfig(
+            guid='Hikrobot-MV-CU013-80GC-DA8386093',
+            frame_id='camera_pano_sw',
+            stream_name='pano_sw_raw'
+        ),
+        CameraConfig(
+            guid='Hikrobot-MV-CU013-80GC-DA8386110',
+            frame_id='camera_pano_ww',
+            stream_name='pano_ww_raw'
+        ),
+        CameraConfig(
+            guid='Hikrobot-MV-CU013-80GC-DA8386109',
+            frame_id='camera_pano_nw',
+            stream_name='pano_nw_raw'
+        ),
+        CameraConfig(
+            guid='Hikrobot-MV-CU013-80GC-DA8386098',
+            frame_id='camera_pano_nn',
+            stream_name='pano_nn_raw'
+        ),
+    ]
+
+    nodes = []
+
+    for config in configs:
+        node = Node(
+            name=f'camera_driver_{config.frame_id}',
+            package='camera_aravis2',
+            executable='camera_driver_gv',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                {
+                    'guid': config.guid,
+                    'frame_id': config.frame_id,
+                    'stream_names': [config.stream_name],
+                    'camera_info_urls': [os.path.join(
+                        get_package_share_directory('multi_hikrobot'),
+                        f'config/{config.frame_id}.yaml')],
+                    'verbose': False,
+                    'DeviceControl': {},
+                    'TransportLayerControl': {
+                        'GevSCPSPacketSize': 9156
+                    },
+                    'ImageFormatControl': {},
+                    'AcquisitionControl': {
+                        'ExposureTime': 8000.0,
+                        'ExposureAuto': 'Off',
+                        'AcquisitionFrameRateEnable': True,
+                        'AcquisitionFrameRate': 30.0
+                    },
+                }
+            ]
+        )
+        nodes.append(node)
+
+    return LaunchDescription(nodes)
