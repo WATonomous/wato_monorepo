4382d6db0fa8b677e2f4c07c54d17db7a1f33354
Eddy Zhou <edsteredward@gmail.com>
2026-01-17 14:32:27 -0500

    test files

---
+++ src/simulation/carla_ros_bridge/carla_control/test/test_carla_control.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2025-present WATonomous. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit tests for carla_control package."""
+
+
+class TestCarlaControl:
+    """Test suite for carla_control package."""
+
+    def test_placeholder(self):
+        """Placeholder test - replace with actual unit tests."""
+        # TODO: Add unit tests for carla_control
+        pass
