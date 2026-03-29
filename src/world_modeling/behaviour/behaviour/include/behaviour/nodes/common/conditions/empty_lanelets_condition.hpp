a6b990b7bab8a8f8b77b199f09836251a1d0335b
Stephen Huang <jstephhuang@gmail.com>
2026-03-29 12:10:15 -0400

    [Behaviour] Service to reset bt and fix reg elem latch (#406)
    * goal point instead of lanelet
    
    * traffic light setup still need testing
    
    * stop sign and traffic light untested
    
    * stop sign fix
    
    * traffic light right turn
    
    * traffic light right turn latch untested
    
    * right of way
    
    * stop line missing threshold param m
    
    * services and fixed stupid stop sign latch
    
    * serivce name in laucn and docs
    
    * pre-commit

--- src/world_modeling/behaviour/behaviour/include/behaviour/nodes/common/conditions/empty_lanelets_condition.hpp
+++ src/world_modeling/behaviour/behaviour/include/behaviour/nodes/common/conditions/empty_lanelets_condition.hpp
@@ -12,33 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef BEHAVIOUR__NODES__INTERSECTION__ACTIONS__GET_STOP_SIGN_CARS_ACTION_HPP_
-#define BEHAVIOUR__NODES__INTERSECTION__ACTIONS__GET_STOP_SIGN_CARS_ACTION_HPP_
+#ifndef BEHAVIOUR__NODES__COMMON__CONDITIONS__EMPTY_LANELETS_CONDITION_HPP_
+#define BEHAVIOUR__NODES__COMMON__CONDITIONS__EMPTY_LANELETS_CONDITION_HPP_
 
-#include <behaviortree_cpp/action_node.h>
+#include <behaviortree_cpp/condition_node.h>
 
-#include <cstddef>
 #include <cstdint>
 #include <iostream>
 #include <string>
-#include <unordered_set>
 #include <vector>
 
 #include "behaviour/nodes/bt_logger_base.hpp"
-#include "behaviour/utils/utils.hpp"
+#include "behaviour/utils/lanelet.hpp"
+#include "behaviour/utils/ports.hpp"
+#include "behaviour/utils/world_objects.hpp"
 #include "world_model_msgs/msg/world_object.hpp"
 
 namespace behaviour
 {
-/**
-   * @class GetStopSignCarsAction
-   * @brief SyncActionNode to collect car IDs occupying relevant right-of-way lanelets.
-   */
-class GetStopSignCarsAction : public BT::SyncActionNode, protected BTLoggerBase
+class EmptyLaneletsCondition : public BT::ConditionNode, protected BTLoggerBase
 {
 public:
-  GetStopSignCarsAction(const std::string & name, const BT::NodeConfig & config, const rclcpp::Logger & logger)
-  : BT::SyncActionNode(name, config)
+  EmptyLaneletsCondition(const std::string & name, const BT::NodeConfig & config, const rclcpp::Logger & logger)
+  : BT::ConditionNode(name, config)
   , BTLoggerBase(logger)
   {}
 
@@ -48,8 +44,6 @@ public:
       BT::InputPort<std::vector<int64_t>>("lanelet_ids"),
       BT::InputPort<std::vector<world_model_msgs::msg::WorldObject>>("objects"),
       BT::InputPort<std::size_t>("hypothesis_index"),
-      BT::OutputPort<std::vector<std::string>>("out_stop_sign_car_ids"),
-      BT::OutputPort<std::vector<world_model_msgs::msg::WorldObject>>("out_stop_sign_cars"),
     };
   }
 
@@ -64,6 +58,11 @@ public:
       return BT::NodeStatus::FAILURE;
     }
 
+    if (lanelet_ids->empty()) {
+      RCLCPP_DEBUG_STREAM(logger(), "empty_lanelet_ids");
+      return BT::NodeStatus::FAILURE;
+    }
+
     auto objects = ports::tryGet<std::vector<world_model_msgs::msg::WorldObject>>(*this, "objects");
     if (!ports::require(objects, "objects", missing_input_callback)) {
       return BT::NodeStatus::FAILURE;
@@ -74,31 +73,24 @@ public:
       return BT::NodeStatus::FAILURE;
     }
 
-    std::vector<std::string> out_ids;
-    std::vector<world_model_msgs::msg::WorldObject> out_cars;
-    std::unordered_set<std::string> seen_ids;
-    for (const auto lanelet_id : *lanelet_ids) {
-      const auto cars_in_lanelet = utils::world_objects::getCarsByLanelet(*objects, *hypothesis_index, lanelet_id);
-
-      for (const auto * obj : cars_in_lanelet) {
-        if (obj == nullptr) {
-          continue;
-        }
-
-        if (!seen_ids.insert(obj->detection.id).second) {
-          continue;
-        }
-
-        out_ids.push_back(obj->detection.id);
-        out_cars.push_back(*obj);
+    for (const auto & object : *objects) {
+      const auto object_lanelet_id = object.lanelet_ahead.current_lanelet_id;
+      if (!utils::lanelet::containsLaneletId(*lanelet_ids, object_lanelet_id)) {
+        continue;
       }
+
+      if (!utils::world_objects::isRoadUser(object, *hypothesis_index)) {
+        continue;
+      }
+
+      RCLCPP_DEBUG_STREAM(
+        logger(), "lanelet_occupied lanelet_id=" << object_lanelet_id << " object_id=" << object.detection.id);
+      return BT::NodeStatus::FAILURE;
     }
-    setOutput("out_stop_sign_car_ids", out_ids);
-    setOutput("out_stop_sign_cars", out_cars);
+
     return BT::NodeStatus::SUCCESS;
   }
 };
-
 }  // namespace behaviour
 
-#endif  // BEHAVIOUR__NODES__INTERSECTION__ACTIONS__GET_STOP_SIGN_CARS_ACTION_HPP_
+#endif  // BEHAVIOUR__NODES__COMMON__CONDITIONS__EMPTY_LANELETS_CONDITION_HPP_
