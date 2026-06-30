f3e190d037e8f93c894845da9bc74177ce36dfbb
RyanL2 <ryleitianebao@gmail.com>
2026-06-30 03:51:45 -0400

    fix(prediction_ml): remove stray patch artifacts

--- src/world_modeling/prediction_ml/src/mtr_scene_adapter.cpp
+++ src/world_modeling/prediction_ml/src/mtr_scene_adapter.cpp
@@ -50,18 +50,3 @@ deep_msgs::msg::MtrScene MtrSceneAdapter::build(const vision_msgs::msg::Detectio
   return scene;
 }
 }  // namespace prediction_ml
-// Copyright (c) 2025-present WATonomous. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-@@
